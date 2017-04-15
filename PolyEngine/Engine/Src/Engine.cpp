a7b36b24429e70be4e046db48de40f40e4b92123
vadordev <msidorcz@gmail.com>
2017-04-15 15:42:19 +0200

    Implemented basic Pre/Main/Post phase update functionality along with simple Register functions.

--- PolyEngine/Engine/Src/Engine.cpp
+++ PolyEngine/Engine/Src/Engine.cpp
@@ -2,34 +2,89 @@
 
 using namespace Poly;
 
-Engine::Engine(IGame* game) : Game(game) {
+Engine::Engine(IGame* game) : Game(game) 
+{
 	Game->RegisterEngine(this);
 	Renderer = CreateRenderingContext();
 }
 
-bool Engine::Init(const IRenderingContextParams* context) {
+bool Engine::Init(const IRenderingContextParams* context) 
+{
 	if (!Renderer->Init(context))
 		return false;
 	Game->Init();
 	return true;
 }
 
-void Poly::Engine::Deinit()
+void Engine::Deinit()
 {
 	Renderer->Deinit();
 	delete Renderer;
 	Renderer = nullptr;
 }
 
+void Engine::RegisterPreupdatePhase(PhaseUpdateFunction phaseFunction)
+{
+	for (auto& iter : GamePreupdatePhases)
+	{
+		if (iter.target<PhaseUpdateFunction>() == phaseFunction.target<PhaseUpdateFunction>())
+			return;
+	}
+	GamePreupdatePhases.PushBack(phaseFunction);
+}
+
+void Engine::RegisterUpdatePhase(PhaseUpdateFunction phaseFunction)
+{
+	for (auto& iter : GameUpdatePhases)
+	{
+		if (iter.target<PhaseUpdateFunction>() == phaseFunction.target<PhaseUpdateFunction>())
+			return;
+	}
+	GameUpdatePhases.PushBack(phaseFunction);
+}
+
+void Engine::RegisterPostupdatePhase(PhaseUpdateFunction phaseFunction)
+{
+	for (auto& iter : GamePostupdatePhases)
+	{
+		if (iter.target<PhaseUpdateFunction>() == phaseFunction.target<PhaseUpdateFunction>())
+			return;
+	}
+	GamePostupdatePhases.PushBack(phaseFunction);
+}
+
 // temporary include, remove this ASAP
 #include <gl/glew.h>
 
-void Engine::Update(float dt) {
-	Game->Update(dt);
+void Engine::Update(float dt)
+{
+	PreupdatePhases();
+
+	UpdatePhases();
+
+	PostupdatePhases();
 
 	// very simple temporary loop, this should be moved somwhere else
 	glClearColor(0.0f, 0.2f, 0.4f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	Renderer->EndFrame();
+}
+
+void Engine::PreupdatePhases()
+{
+	for (auto& update : GamePreupdatePhases)
+		update(&GetWorld());
+}
+
+void Engine::UpdatePhases()
+{
+	for (auto& update : GameUpdatePhases)
+		update(&GetWorld());
+}
+
+void Engine::PostupdatePhases()
+{
+	for (auto& update : GamePostupdatePhases)
+		update(&GetWorld());
 }
--- PolyEngine/Engine/Src/Engine.hpp
+++ PolyEngine/Engine/Src/Engine.hpp
@@ -1,31 +1,72 @@
 #pragma once
 
+#include <functional>
 #include <Core.hpp>
 #include "RenderingContext.hpp"
 #include "World.hpp"
+#include "SystemBase.hpp"
 
-namespace Poly {
+namespace Poly 
+{
 	class Engine;
+	typedef std::function<void(World*)> PhaseUpdateFunction;
 
-	class ENGINE_DLLEXPORT IGame : public BaseObject<> {
+	class ENGINE_DLLEXPORT IGame : public BaseObject<> 
+	{
 	public:
 		virtual void RegisterEngine(Engine* engine) = 0;
 		virtual void Init() = 0;
 		virtual void Update(float dt) = 0;
 	};
 
-	class ENGINE_DLLEXPORT Engine : public BaseObject<> {
+	class ENGINE_DLLEXPORT Engine : public BaseObject<> 
+	{
 	public:
 		Engine(IGame* game);
 		bool Init(const IRenderingContextParams* context);
 		void Deinit();
 
+		//////////////////////////////
+		/// Registers a PhaseUpdateFunction to be executed in the Preupdate 
+		/// part of a single frame in the same order as they were passed in.
+		/// @see RegisterUpdatePhase()
+		/// @see RegisterPostupdatePhase()
+		/// @param phaseFunction - void function(World*)
+		//////////////////////////////
+		void RegisterPreupdatePhase(PhaseUpdateFunction phaseFunction);
+
+		//////////////////////////////
+		/// Registers a PhaseUpdateFunction to be executed in the main update 
+		/// part of a single frame in the same order as they were passed in.
+		/// @see RegisterPreupdatePhase()
+		/// @see RegisterPostupdatePhase()
+		/// @param phaseFunction - void function(World*)
+		//////////////////////////////
+		void RegisterUpdatePhase(PhaseUpdateFunction phaseFunction);
+
+		//////////////////////////////
+		/// Registers a PhaseUpdateFunction to be executed in the Postupdate 
+		/// part of a single frame in the same order as they were passed in.
+		/// @see RegisterPreupdatePhase()
+		/// @see RegisterUpdatePhase()
+		/// @param phaseFunction - void function(World*)
+		//////////////////////////////
+		void RegisterPostupdatePhase(PhaseUpdateFunction phaseFunction);
+
+
 		void Update(float dt);
 
 		World& GetWorld() { return BaseWorld; }
 	private:
+		inline void PreupdatePhases();
+		inline void UpdatePhases();
+		inline void PostupdatePhases();
+
 		World BaseWorld;
 		IGame* Game;
 		IRenderingContext* Renderer;
+		Dynarray< PhaseUpdateFunction > GamePreupdatePhases;
+		Dynarray< PhaseUpdateFunction > GameUpdatePhases;
+		Dynarray< PhaseUpdateFunction > GamePostupdatePhases;
 	};
 }
