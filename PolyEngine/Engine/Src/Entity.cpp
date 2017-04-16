88f64bbfb815f1a35943d59774420afde0f8c814
vadordev <msidorcz@gmail.com>
2017-04-16 14:01:03 +0200

    Implemented GetSibling() for components with a bunch of other component getters. Implemented ComponentPosessionFlags for Entities, allowing for fast and efficient checks.

--- PolyEngine/Engine/Src/Entity.cpp
+++ PolyEngine/Engine/Src/Entity.cpp
@@ -3,7 +3,13 @@
 using namespace Poly;
 
 Entity::Entity(const World * world)
-: EntityID(UniqueID::Generate()), EntityWorld(world)
+: EntityID(UniqueID::Generate()), EntityWorld(world), ComponentPosessionFlags(0)
 {
 	memset(Components, 0, sizeof(ComponentBase*) * MAX_COMPONENTS_COUNT);
+}
+
+bool Entity::HasComponent(size_t ID) const
+{
+	HEAVY_ASSERTE(ID < MAX_COMPONENTS_COUNT, "Invalid component ID - greater than MAX_COMPONENTS_COUNT.");
+	return ComponentPosessionFlags[ID];
 }
