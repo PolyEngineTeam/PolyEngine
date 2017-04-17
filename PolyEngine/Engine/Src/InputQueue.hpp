894ec786265ab95a21f895ca60b2a42e10348ca6
Michal Majczak <michal.majczak92@gmail.com>
2017-04-17 12:53:17 +0200

    InputEvent fix.

--- PolyEngine/Engine/Src/InputQueue.hpp
+++ PolyEngine/Engine/Src/InputQueue.hpp
@@ -11,25 +11,19 @@ namespace Poly
 		KEYDOWN,
 		KEYUP,
 		MOUSEMOVE,
-		WHEELMOVE
+		WHEELMOVE,
+		_COUNT
 	};
 
 	struct InputEvent : public BaseObject<>
 	{	
-		InputEvent() {}
-		InputEvent(const InputEvent &src) : Type(src.Type), Pos(src.Pos) {}
+		InputEvent() = default;
 		InputEvent(eEventType type, eKey key) : Type(type), Key(key) {}
 		InputEvent(eEventType type, const Vector& pos) : Type(type), Pos(pos) {}
-		~InputEvent() {}
 
-		InputEvent& operator=(const InputEvent& src) { Type = src.Type; Pos = src.Pos; return *this; }
-
-		eEventType Type;
-		union
-		{
-			eKey Key;
-			Vector Pos;
-		};
+		eEventType Type = eEventType::_COUNT;
+		eKey Key;
+		Vector Pos;
 	};
 
 	typedef Queue<InputEvent> InputQueue;
