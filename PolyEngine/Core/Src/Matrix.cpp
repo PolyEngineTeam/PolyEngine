dc3980b8d846ed82ca046c9813973be852b3e134
Michal Majczak <michal.majczak92@gmail.com>
2017-04-21 23:26:20 +0200

    Minor comment.

--- PolyEngine/Core/Src/Matrix.cpp
+++ PolyEngine/Core/Src/Matrix.cpp
@@ -368,6 +368,7 @@ bool Matrix::Decompose(Vector& translation, Quaternion& rotation, Vector& scale)
 {
 	MatrixSkew skew;
 	Vector perspPoint;
+	//TODO optimize this!
 	bool result = Decompose(translation, rotation, scale, skew, perspPoint);
 	ASSERTE(!result || (Cmpf(skew.XY, 0) && Cmpf(skew.XZ, 0) && Cmpf(skew.YZ, 0)), "Non zero skew, use the other overload of the method!");
 	ASSERTE(!result || perspPoint != Vector(0,0,0), "Non zero perspective, use the other overload of the method!");
