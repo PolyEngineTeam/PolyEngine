56ca8824e761f3f496819a9406dcf983f0e59e0f
R4yRayR4y <klusek.raf@gmail.com>
2017-04-22 19:19:12 +0200

    TransformComponent implementation (#7)
    * Basic TransComp
    
    * TransComp position interface
    
    * TransComp renamed to TransformComponent
    
    * TransformationComponent matrix splitted into separate values (translation, rotation, scale)
    
    * TransformComponent review updates
    
    * .vcxproj files
    
    * TransformComponent - UpdateGlobalTransformationMatrix with respect to parents
    
    * UpdateGlobalTransformationMatrix fix
    
    * UpdateGlobalTransformationMatrix fix #2
    
    * TransformComponent review updates
    
    * Parent is now not const
    
    * TransformComponent Children and destructor
    
    * TransformationComponent - proper SetParent implementation
    
    * Review updates

---
+++ PolyEngine/Engine/Src/TransformComponent.cpp
@@ -0,0 +1,157 @@
+#include "TransformComponent.hpp"
+
+using namespace Poly;
+
+//-----------------------------------------------------------------------------
+TransformComponent::~TransformComponent() {
+	if (Parent != nullptr)
+	{
+		Parent->Children.Remove(this);
+	}
+}
+
+//-----------------------------------------------------------------------------
+void TransformComponent::SetParent(TransformComponent* parent) 
+{
+	if (parent != nullptr)
+	{
+		parent->UpdateGlobalTransformationCache();
+		LocalTranslation = LocalTranslation - parent->GlobalTranslation;
+		LocalRotation = LocalRotation * parent->GlobalRotation.GetConjugated();
+		Vector parentGlobalScale = parent->GlobalScale;
+		LocalScale.X = LocalScale.X / parentGlobalScale.X;
+		LocalScale.Y = LocalScale.Y / parentGlobalScale.Y;
+		LocalScale.Z = LocalScale.Z / parentGlobalScale.Z;
+		UpdateLocalTransformationCache();
+
+		Parent = parent;
+		Parent->Children.PushBack(this);
+	}
+	else 
+	{ 
+		ResetParent();
+	}
+}
+
+//-----------------------------------------------------------------------------
+void TransformComponent::ResetParent()
+{
+	ASSERTE(Parent, "ResetParent() called with parent == nullptr");
+	Matrix globalTransform = GetGlobalTransformationMatrix();
+	Parent->Children.Remove(this);
+	Parent = nullptr;
+	SetLocalTransformationMatrix(globalTransform);
+}
+
+//------------------------------------------------------------------------------
+const Vector& TransformComponent::GetGlobalTranslation() const
+{
+	UpdateGlobalTransformationCache();
+	return GlobalTranslation;
+}
+
+//------------------------------------------------------------------------------
+void TransformComponent::SetLocalTranslation(const Vector& position) 
+{
+	LocalTranslation = position;
+	LocalDirty = true;
+	GlobalDirty = true;
+}
+
+//------------------------------------------------------------------------------
+const Quaternion& TransformComponent::GetGlobalRotation() const
+{
+	UpdateGlobalTransformationCache();
+	return GlobalRotation;
+}
+
+//------------------------------------------------------------------------------
+void TransformComponent::SetLocalRotation(const Quaternion& quaternion) 
+{ 
+	LocalRotation = quaternion;
+	LocalDirty = true;
+	GlobalDirty = true;
+}
+
+//------------------------------------------------------------------------------
+const Vector& TransformComponent::GetGlobalScale() const
+{
+	UpdateGlobalTransformationCache();
+	return GlobalScale;
+}
+
+//------------------------------------------------------------------------------
+void TransformComponent::SetLocalScale(const Vector& scale) 
+{
+	LocalScale = scale;
+	LocalDirty = true;
+	GlobalDirty = true;
+}
+
+//------------------------------------------------------------------------------
+const Matrix& TransformComponent::GetLocalTransformationMatrix() const 
+{
+	UpdateLocalTransformationCache();
+	return LocalTransform;
+}
+
+//------------------------------------------------------------------------------
+const Matrix& TransformComponent::GetGlobalTransformationMatrix() const 
+{
+	UpdateGlobalTransformationCache();
+	return GlobalTransform;
+}
+
+//------------------------------------------------------------------------------
+void TransformComponent::SetLocalTransformationMatrix(const Matrix& localTransformation)
+{
+	LocalTransform = localTransformation;
+	localTransformation.Decompose(LocalTranslation, LocalRotation, LocalScale);
+	LocalDirty = false;
+	GlobalDirty = true;
+}
+
+//------------------------------------------------------------------------------
+const bool TransformComponent::UpdateLocalTransformationCache() const
+{
+	if (LocalDirty)
+	{
+		Matrix translation;
+		Matrix rotation = LocalRotation.ToRotationMatrix();
+		Matrix scale;
+		translation.SetTranslation(LocalTranslation);
+		scale.SetScale(LocalScale);
+		LocalTransform = translation * rotation * scale;
+		LocalDirty = false;
+		return true;
+	}
+	return false;
+}
+
+//------------------------------------------------------------------------------
+const bool TransformComponent::UpdateGlobalTransformationCache() const 
+{
+	if (Parent == nullptr) 
+	{
+		if (GlobalDirty) {
+			GlobalTransform = GetLocalTransformationMatrix();
+			GlobalTransform.Decompose(GlobalTranslation, GlobalRotation, GlobalScale);
+			GlobalDirty = false;
+			return true;
+		} 
+		else 
+			return false;
+	}
+	else 
+	{
+		if (Parent->UpdateGlobalTransformationCache() || GlobalDirty) 
+		{
+			GlobalTransform = Parent->GlobalTransform * GetLocalTransformationMatrix();
+			GlobalTransform.Decompose(GlobalTranslation, GlobalRotation, GlobalScale);
+			GlobalDirty = false;
+			return true;
+		} 
+		else 
+			return false;
+	}
+}
