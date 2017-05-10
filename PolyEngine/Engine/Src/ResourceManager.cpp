ab75680a15c46680604b68c9763a7362f5085b27
Unknown <klusek.raf@gmail.com>
2017-05-10 21:35:40 +0200

    GetResourcesAbsolutePath() temporary implementation

--- PolyEngine/Engine/Src/ResourceManager.cpp
+++ PolyEngine/Engine/Src/ResourceManager.cpp
@@ -2,12 +2,35 @@
 
 #include "ResourceManager.hpp"
 
+#include <fstream>
+
 using namespace Poly;
 
 
 const String& Poly::GetResourcesAbsolutePath()
 {
 	static String PATH;
-	// TODO implement
+
+	//TODO temporary implementation. should use some JSON config file
+	String defaultPath("C:\\Polyengine\\Resources");
+	std::string tmpConfigPath = "tmp_config.txt";
+
+	std::ifstream file;
+	file.open(tmpConfigPath);
+	if (file.good())
+	{
+		file.close();
+		PATH = LoadTextFile(String(tmpConfigPath.c_str()));
+	}
+	else
+	{
+		file.close();
+		std::ofstream ofile;
+		ofile.open(tmpConfigPath);
+		ofile << defaultPath;
+		PATH = defaultPath;
+	}
+	file.close();
+
 	return PATH;
 }
