9ae6025159a3292ee88f216be2392907f91286e4
Althiena <althienao@gmail.com>
2017-05-25 15:51:06 +0200

    Queue implementation based on Dynarray (#32)
    * Queue implementation based on Dynarray
    
    * Deffered memory allocation in queue
    
    * Improved reallocation conditions
    
    * Simplified reallocations conditions. Additional copy ctor debug.

--- PolyEngine/Core/Src/Dynarray.hpp
+++ PolyEngine/Core/Src/Dynarray.hpp
@@ -1,62 +1,11 @@
 #pragma once
 
+#include "ObjectLifetimeHelpers.hpp"
 #include "Defines.hpp"
 #include "Allocator.hpp"
 
-namespace Poly {
-
-	namespace ObjectLifetimeHelper
-	{
-		// Not really sure if this is needed
-		template<class T>
-		void DefaultCreate(T* ptr, typename std::enable_if<std::is_trivially_default_constructible<T>::value>::type* = 0)
-		{
-			::new (ptr)T; // TODO use memcpy here
-		}
-
-		template<class T>
-		void DefaultCreate(T* ptr, typename std::enable_if<!std::is_trivially_default_constructible<T>::value>::type* = 0)
-		{
-			::new (ptr)T;
-		}
-
-		template<class T>
-		void CopyCreate(T* ptr, const T& obj, typename std::enable_if<std::is_trivially_copy_constructible<T>::value>::type* = 0)
-		{
-			::new (ptr)T(obj); // TODO use memcpy here
-		}
-
-		template<class T>
-		void CopyCreate(T* ptr, const T& obj, typename std::enable_if<!std::is_trivially_copy_constructible<T>::value>::type* = 0)
-		{
-			::new (ptr)T(obj);
-		}
-
-		template<class T>
-		void MoveCreate(T* ptr, T&& obj, typename std::enable_if<std::is_trivially_move_constructible<T>::value>::type* = 0)
-		{
-			::new (ptr)T(std::move(obj)); // TODO use memcpy here
-		}
-
-		template<class T>
-		void MoveCreate(T* ptr, T&& obj, typename std::enable_if<!std::is_trivially_move_constructible<T>::value>::type* = 0)
-		{
-			::new (ptr)T(std::move(obj));
-		}
-
-		template<class T>
-		void Destroy(T* t, typename std::enable_if<std::is_trivially_destructible<T>::value>::type* = 0)
-		{
-			t->~T(); // calling destructor here is unnecessary
-		}
-
-		template<class T>
-		void Destroy(T* t, typename std::enable_if<!std::is_trivially_destructible<T>::value>::type* = 0)
-		{
-			t->~T();
-		}
-	}
-
+namespace Poly 
+{
 	template<typename T>
 	class Dynarray : public BaseObject<>
 	{
--- PolyEngine/Engine/Src/InputSystem.cpp
+++ PolyEngine/Engine/Src/InputSystem.cpp
@@ -15,7 +15,7 @@ void InputSystem::InputPhase(World* world)
 
 	InputQueue& InputEventsQueue = world->GetEngine()->GetInputQueue();
 
-	while (!InputEventsQueue.Empty())
+	while (!InputEventsQueue.IsEmpty())
 	{
 		InputEvent& ev = InputEventsQueue.Front();
 		switch (ev.Type)
@@ -40,7 +40,7 @@ void InputSystem::InputPhase(World* world)
 			HEAVY_ASSERTE(false, "_COUNT enum value passed to InputEventQueue::Push(), which is an invalid value");
 			break;
 		}
-		InputEventsQueue.Pop();
+		InputEventsQueue.PopBack();
 	}
 
 
