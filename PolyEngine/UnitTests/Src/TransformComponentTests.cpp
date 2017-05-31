db2fd7e018b2806ada5a0c0f545fceedd96023c7
Unknown <klusek.raf@gmail.com>
2017-05-31 12:00:47 +0200

    fix

--- PolyEngine/UnitTests/Src/TransformComponentTests.cpp
+++ PolyEngine/UnitTests/Src/TransformComponentTests.cpp
@@ -130,3 +130,26 @@ TEST_CASE("Multi-layer hierarchy", "[TransformComponent]")
 	REQUIRE(Cmpf(tc6.GetGlobalRotation().W, rotation.W));
 	REQUIRE(tc6.GetGlobalScale() == scale);
 }
+
+TEST_CASE("Multiple transformation changes", "[TransformComponent]")
+{
+	TransformComponent tc1, tc2;
+	Vector v1 = Vector(1, 1, 1);
+	Vector v2 = Vector(2, 2, 2);
+	Vector v3 = Vector(3, 3, 3);
+
+	tc2.SetParent(&tc1);
+
+	tc1.SetLocalTranslation(v1);
+	tc1.GetGlobalTranslation();
+	REQUIRE(tc2.GetGlobalTranslation() == v1);
+	REQUIRE(tc2.GetGlobalTransformationMatrix().Data[3] == v1.X);
+	tc1.SetLocalTranslation(v2);
+	tc1.GetGlobalTransformationMatrix();
+	REQUIRE(tc2.GetGlobalTranslation() == v2);
+	REQUIRE(tc2.GetGlobalTransformationMatrix().Data[3] == v2.X);
+	tc1.SetLocalTranslation(v3);
+	tc1.GetGlobalTransformationMatrix();
+	REQUIRE(tc2.GetGlobalTranslation() == v3);
+	REQUIRE(tc2.GetGlobalTransformationMatrix().Data[3] == v3.X);
+}
