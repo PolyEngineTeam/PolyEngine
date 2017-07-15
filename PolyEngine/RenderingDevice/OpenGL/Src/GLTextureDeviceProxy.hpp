ab7b21c3c5b9c8dfc5681a630c5556accf2a76a9
Michal Majczak <michal.majczak92@gmail.com>
2017-07-15 19:25:31 +0200

    Proxies improvements

--- PolyEngine/RenderingDevice/OpenGL/Src/GLTextureDeviceProxy.hpp
+++ PolyEngine/RenderingDevice/OpenGL/Src/GLTextureDeviceProxy.hpp
@@ -8,16 +8,21 @@ namespace Poly
 	class GLTextureDeviceProxy : public ITextureDeviceProxy
 	{
 	public:
+		GLTextureDeviceProxy(size_t width, size_t height, GLuint internalFormat);
 		GLTextureDeviceProxy(size_t width, size_t height, eTextureUsageType usage);
 		virtual ~GLTextureDeviceProxy();
 
 		void SetContent(eTextureDataFormat inputFormat, const unsigned char* data) override;
 		void SetSubContent(size_t width, size_t height, size_t offsetX, size_t offsetY, eTextureDataFormat format, const unsigned char* data) override;
 
+		GLuint GetTextureID() const { return TextureID; }
 	private:
+		void InitTextureParams();
+
 		size_t Width = 0;
 		size_t Height = 0;
 		GLuint TextureID = 0;
+		GLuint InternalFormat;
 		eTextureUsageType Usage = eTextureUsageType::_COUNT;
 
 		friend class GLRenderingDevice;
