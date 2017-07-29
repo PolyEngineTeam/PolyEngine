2023df237a015e77eaa5d6e053675174e1190b97
Squares <m.piotr@moscicki.pl>
2017-07-29 08:40:34 +0200

    Added test levels.

---
+++ PolyEngine/Game/Res/Levels/TilesPalette.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TilePalette" tilewidth="32" tileheight="32" tilecount="480" columns="30">
+ <image source="tiles1.png" width="960" height="540"/>
+ <tile id="0" type="ground"/>
+ <tile id="1" type="playerstart position"/>
+ <tile id="2" type="lava"/>
+ <tile id="3" type="blue potion"/>
+</tileset>
