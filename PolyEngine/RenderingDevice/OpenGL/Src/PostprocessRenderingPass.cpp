7cb4c2722b6c32feebcbba668b7cd64f03ed9084
Michal Klos <michal.m.klos@gmail.com>
2017-07-29 23:35:57 +0200

    removed debug  log each frame

--- PolyEngine/RenderingDevice/OpenGL/Src/PostprocessRenderingPass.cpp
+++ PolyEngine/RenderingDevice/OpenGL/Src/PostprocessRenderingPass.cpp
@@ -110,7 +110,7 @@ void PostprocessRenderingPass::OnRun(World* world, const CameraComponent* camera
 		GetProgram().SetUniform("uStripesPower",					PostprocessSettings->StripesPower);
 			
 		
-		gConsole.LogInfo("void PostprocessRenderingPass::OnRun: UseCashetes: {}", PostprocessSettings->UseCashetes);
+		// gConsole.LogInfo("void PostprocessRenderingPass::OnRun: UseCashetes: {}", PostprocessSettings->UseCashetes);
 	}
 
 
