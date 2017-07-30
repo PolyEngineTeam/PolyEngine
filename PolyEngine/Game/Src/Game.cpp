d6e5e16bbf55d9c1a564e9d98379de1d3fd0a285
Squares <m.piotr@moscicki.pl>
2017-07-30 02:13:05 +0200

    Powerups fizes

--- PolyEngine/Game/Src/Game.cpp
+++ PolyEngine/Game/Src/Game.cpp
@@ -51,7 +51,7 @@ void SGJGame::Init()
 	DeferredTaskSystem::AddComponent<PostprocessSettingsComponent>(gEngine->GetWorld(), Camera);
 	
 	// load levels
-	SGJ::GameManagerSystem::LoadLevel(gEngine->GetWorld(), "Levels/Level1.csv");
+	SGJ::GameManagerSystem::LoadLevel(gEngine->GetWorld(), "Levels/Level3.csv");
 	SGJ::GameManagerSystem::SpawnLevel(gEngine->GetWorld(), 0);
 };
 
