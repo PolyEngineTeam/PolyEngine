2c2341013c9e5d8125574aa47bffaddda9a9f70a
Squares <m.piotr@moscicki.pl>
2017-07-30 02:48:29 +0200

    Switched map.

--- PolyEngine/Game/Src/Game.cpp
+++ PolyEngine/Game/Src/Game.cpp
@@ -51,7 +51,7 @@ void SGJGame::Init()
 	DeferredTaskSystem::AddComponent<PostprocessSettingsComponent>(gEngine->GetWorld(), Camera);
 	
 	// load levels
-	SGJ::GameManagerSystem::LoadLevel(gEngine->GetWorld(), "Levels/Level4.csv");
+	SGJ::GameManagerSystem::LoadLevel(gEngine->GetWorld(), "Levels/Level3.csv");
 	SGJ::GameManagerSystem::SpawnLevel(gEngine->GetWorld(), 0);
 
 	JumpSoundPreload = ResourceManager<SoundResource>::Load("Audio/jump-sound.ogg", eResourceSource::GAME);
