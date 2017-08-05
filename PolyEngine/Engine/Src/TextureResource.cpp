176b599e3630eeab3e316cc0d6f6086540fa2f73
Michal 'spolsh' Kłoś <michal.m.klos@gmail.com>
2017-08-05 11:55:36 +0200

    fixed texture flipped Y issue

--- PolyEngine/Engine/Src/TextureResource.cpp
+++ PolyEngine/Engine/Src/TextureResource.cpp
@@ -16,15 +16,16 @@ TextureResource::TextureResource(const String& path)
 		throw ResourceLoadFailedException();
 	}
 
+	int LoadedChannels = 4;
 	// Flip Y axis
-	// int rowSize = Width*Channels;
-	// static Dynarray<unsigned char> row;
-	// row.Resize(rowSize);
-	// for (int i = 0; i < Height/2; ++i) {
-	// 	memcpy(row.GetData(), Image + ((Height - i - 1) * Width*Channels), sizeof(unsigned char) * rowSize);
-	// 	memcpy(Image + ((Height - i - 1) * Width*Channels), Image + (i * Width*Channels), sizeof(unsigned char) * rowSize);
-	// 	memcpy(Image + (i * Width*Channels), row.GetData(), sizeof(unsigned char) * rowSize);
-	// }
+	int rowSize = Width*LoadedChannels;
+	static Dynarray<unsigned char> row;
+	row.Resize(rowSize);
+	for (int i = 0; i < Height/2; ++i) {
+		memcpy(row.GetData(),									  Image + ((Height - i - 1) * Width*LoadedChannels),	sizeof(unsigned char) * rowSize);
+		memcpy(Image + ((Height - i - 1) * Width*LoadedChannels), Image + (i * Width*LoadedChannels),					sizeof(unsigned char) * rowSize);
+		memcpy(Image + (i * Width*LoadedChannels),				  row.GetData(),										sizeof(unsigned char) * rowSize);
+	}
 
 	TextureProxy = gEngine->GetRenderingDevice()->CreateTexture(Width, Height, eTextureUsageType::DIFFUSE); //HACK, remove deffise from here
 	TextureProxy->SetContent(eTextureDataFormat::RGBA, Image);
