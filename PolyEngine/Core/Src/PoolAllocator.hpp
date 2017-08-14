3654db00cd708f37ece5bb4aa2a52a44243d6944
Vuko Drakkeinen <sneyk8+git@gmail.com>
2017-08-14 21:29:45 +0200

    Make the allocator fully platform-independent (#21)

--- PolyEngine/Core/Src/PoolAllocator.hpp
+++ PolyEngine/Core/Src/PoolAllocator.hpp
@@ -17,7 +17,7 @@ namespace Poly {
 			: Capacity(count), FreeBlockCount(count)
 		{
 			ASSERTE(count > 0, "Cell count cannot be lower than 1.");
-			Data = reinterpret_cast<T*>(DefaultAlloc(sizeof(T) * Capacity));
+			Data = Allocate<T>(Capacity);
 			Next = Data;
 		}
 
@@ -25,7 +25,7 @@ namespace Poly {
 		virtual ~PoolAllocator()
 		{
 			ASSERTE(Data, "Allocator is invalid");
-			DefaultFree(Data);
+			Deallocate(Data);
 			Data = nullptr;
 		}
 
