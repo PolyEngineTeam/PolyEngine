cb692f5fd2a52d344c5c41ecfbeb34165bf18cff
Squares <m.piotr@moscicki.pl>
2017-11-10 09:21:58 +0100

    Added some fields to SoundDataComponent.

--- PolyEngine/SoundDevices/Open AL/ALSoundDevice/Src/SoundDataComponent.hpp
+++ PolyEngine/SoundDevices/Open AL/ALSoundDevice/Src/SoundDataComponent.hpp
@@ -6,7 +6,12 @@ namespace Poly
 {
 	class SoundDataComponent : public SoundDataComponentProxy
 	{
+	public:
 		SoundDataComponent();
 		~SoundDataComponent();
+		
+		unsigned int EmitterID;
+		Poly::Dynarray<unsigned int> QueuedBuffers;
+		unsigned int MaxBuffersInQueue = 2;
 	};
 }
