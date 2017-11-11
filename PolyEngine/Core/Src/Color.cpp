4143b1f1209751baaba53e5c21ac4b26ac86c29f
Michal Klos <michal.m.klos@gmail.com>
2017-11-11 16:36:37 +0100

    moved simple operations to header

--- PolyEngine/Core/Src/Color.cpp
+++ PolyEngine/Core/Src/Color.cpp
@@ -5,37 +5,6 @@
 using namespace Poly;
 
 //------------------------------------------------------------------------------
-Color::Color() : R(0), G(0), B(0), A(1)
-{
-}
-
-//------------------------------------------------------------------------------
-Color::Color(float r, float g, float b, float a)
-	: R(r), G(g), B(b), A(a)
-{
-}
-
-//------------------------------------------------------------------------------
-Color Color::operator+(const Color& rhs) const {
-	return Color(R + rhs.R, G + rhs.G, B + rhs.B, A + rhs.A);
-}
-
-//------------------------------------------------------------------------------
-Color Color::operator-(const Color& rhs) const {
-	return Color(R - rhs.R, G - rhs.G, B - rhs.B, A - rhs.A);
-}
-
-//------------------------------------------------------------------------------
-Color& Color::operator+=(const Color& rhs) {
-	R += rhs.R; G += rhs.G; B += rhs.B; A += rhs.A;
-	return *this;
-}
-
-//------------------------------------------------------------------------------
-Color& Color::operator-=(const Color& rhs) {
-	R -= rhs.R; G -= rhs.G; B -= rhs.B; A -= rhs.A;
-	return *this;
-}
 
 //------------------------------------------------------------------------------
 std::ostream& operator<< (std::ostream& stream, const Color& color)
--- PolyEngine/Core/Src/Color.hpp
+++ PolyEngine/Core/Src/Color.hpp
@@ -9,14 +9,38 @@ namespace Poly {
 	class CORE_DLLEXPORT Color : public BaseObject<>
 	{
 	public:
-		Color();
-		Color(float r, float g, float b, float a = 1.0f);
 
-		// Operators with other Colors
-		Color operator+(const Color& rhs) const;
-		Color operator-(const Color& rhs) const;
-		Color& operator+=(const Color& rhs);
-		Color& operator-=(const Color& rhs);
+		Color() : R(0), G(0), B(0), A(1)
+		{
+		}
+
+		Color(float r, float g, float b, float a = 1.0f) : R(r), G(g), B(b), A(a)
+		{
+		}
+
+		Color operator+(const Color& rhs) const {
+			return Color(R + rhs.R, G + rhs.G, B + rhs.B, A + rhs.A);
+		}
+
+		Color operator-(const Color& rhs) const {
+			return Color(R - rhs.R, G - rhs.G, B - rhs.B, A - rhs.A);
+		}
+
+		Color& operator+=(const Color& rhs) {
+			R += rhs.R; G += rhs.G; B += rhs.B; A += rhs.A;
+			return *this;
+		}
+
+		Color& operator-=(const Color& rhs) {
+			R -= rhs.R; G -= rhs.G; B -= rhs.B; A -= rhs.A;
+			return *this;
+		}
+
+		std::ostream& operator<< (std::ostream& stream, const Color& color)
+		{
+			return stream << "Color[ " << color.R << " " << color.G << " " << color.B << " " << color.A << " ]";
+		}
+
 
 		CORE_DLLEXPORT friend std::ostream& operator<< (std::ostream& stream, const Color& color);
 
