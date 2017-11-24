058574558082c3ed40246c87856f1961903cf1ff
Michał Kłoś <sp0lsh@users.noreply.github.com>
2017-11-24 13:11:00 +0100

    Rendering: Bugfixes (#92)
    * added missing models and placeholder for sponza
    
    * tuned down default post effects
    
    * fixed lighting a bit
    
    * added white dummy texture 1x1 px fallback for diffuse sampler

--- PolyEngine/Engine/Src/PostprocessSettingsComponent.hpp
+++ PolyEngine/Engine/Src/PostprocessSettingsComponent.hpp
@@ -11,16 +11,16 @@ namespace Poly {
 	public:
 
 		bool UseBgShader = true;
-		bool UseFgShader = false;
+		bool UseFgShader = true;
 
 		int UseCashetes = 0;
-		float Distortion = 0.45f;
+		float Distortion = 0.05f;
 		float ColorTempValue = 7500.0f; // 6500.0 from [1000.0, 40000.0]
 		float ColorTempLuminancePreservation = 0.75f;
 		float Saturation = 1.0f;
-		float Grain = 0.1f;
-		float Stripes = 0.25f;
-		float Vignette = 0.30f;
+		float Grain = 0.05f;
+		float Stripes = 0.15f;
+		float Vignette = 0.15f;
 
 	};
 
