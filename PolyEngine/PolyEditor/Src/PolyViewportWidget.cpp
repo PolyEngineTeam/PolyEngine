c8a79ecca6d155d0cec319a2b9417272f7287b2a
Michal Majczak <michal.majczak92@gmail.com>
2017-11-27 20:40:23 +0100

    Cleanup of viewport widget code from Linux broken stuff.

--- PolyEngine/PolyEditor/Src/PolyViewportWidget.cpp
+++ PolyEngine/PolyEditor/Src/PolyViewportWidget.cpp
@@ -1,35 +1,18 @@
 #include "PolyEditorPCH.hpp"
-#include <qtextstream.h>
-#include "PolyViewportWidget.hpp"
 #include <Engine.hpp>
 #include <LibraryLoader.hpp>
-#include <QtGui/QtGui>
 
 
 #if defined(_WIN32)
 	#include <windows.h>
 	#include <windowsx.h>
-#elif defined(__linux__) 
-	#include <QX11Info>
-    #include <X11/Xlib.h>
-    #include <GL/glx.h>
-#elif defined(__APPLE__)
 #else
 #error "Unsupported platform :("
 #endif
 
 extern "C"
 {
-#if defined(_WIN32)
 	using CreateRenderingDeviceFunc = Poly::IRenderingDevice* (HWND hwnd, RECT rect);
-#elif defined(__linux__) 
-	using CreateRenderingDeviceFunc = Poly::IRenderingDevice* (Display* display, Window window, GLXFBConfig fbConfig, const Poly::ScreenSize& size);
-#elif defined(__APPLE__)
-	using CreateRenderingDeviceFunc = Poly::IRenderingDevice* (void* window, const Poly::ScreenSize& size);
-#else
-#error "Unsupported platform :("
-#endif
-	
 	using CreateGameFunc = Poly::IGame* (void);
 }
 
@@ -86,53 +69,8 @@ void PolyViewportWidget::InitializeViewport()
 	std::unique_ptr<Poly::IGame> game = std::unique_ptr<Poly::IGame>(LoadGame());
 #if defined(_WIN32)
 	std::unique_ptr<Poly::IRenderingDevice> device = std::unique_ptr<Poly::IRenderingDevice>(LoadRenderingDevice((HWND)winId(), viewportRect));
-#elif defined(__linux__)
-	//GLXFBConfig config = qglx_findConfig(QX11Info::display(), QX11Info::appScreen(), this->platformWindowFormat());
-
-    //check GLX version
-    {
-        GLint majorGLX = 0, minorGLX = 0;
-        glXQueryVersion(QX11Info::display(), &majorGLX, &minorGLX);
-        if (majorGLX <= 1 && minorGLX < 4) {
-            Poly::gConsole.LogError("GLX 1.4 or greater is required.");
-            std::exit(1);
-        }
-    }
-
-    GLint glxAttribs[] = {
-            GLX_X_RENDERABLE  , True,
-            GLX_DRAWABLE_TYPE , GLX_WINDOW_BIT,
-            GLX_RENDER_TYPE   , GLX_RGBA_BIT,
-            GLX_X_VISUAL_TYPE , GLX_TRUE_COLOR,
-            GLX_RED_SIZE      , 8,
-            GLX_GREEN_SIZE    , 8,
-            GLX_BLUE_SIZE     , 8,
-            GLX_ALPHA_SIZE    , 8,
-            GLX_DEPTH_SIZE    , 24,
-            GLX_STENCIL_SIZE  , 8,
-            GLX_DOUBLEBUFFER  , True,
-            GLX_SAMPLE_BUFFERS, False, //MSAA off
-            GLX_SAMPLES       , 0,
-            None
-    };
-
-    int fbcount = 0;
-    // This part is crashing for now.
-    GLXFBConfig* fbc = glXChooseFBConfig(QX11Info::display(), QX11Info::appScreen(), glxAttribs, &fbcount);
-    if (!fbc) {
-        Poly::gConsole.LogError("Failed to retrieve framebuffer config.");
-        std::exit(1);
-    }
-    GLXFBConfig fbConfig = *fbc;
-    XFree(fbc);
-    Poly::gConsole.LogDebug("Framebuffer config get");
-
-    ASSERTE(fbcount > 0, "Invalid fb count");
-	std::unique_ptr<Poly::IRenderingDevice> device = std::unique_ptr<Poly::IRenderingDevice>(LoadRenderingDevice(QX11Info::display(), (Window)winId(), fbConfig, viewportRect));
-#elif defined(__APPLE__)
-	//TODO
-	// use HIObjectRef(winId())
-	std::unique_ptr<Poly::IRenderingDevice> device = std::unique_ptr<Poly::IRenderingDevice>(LoadRenderingDevice(winId(), viewportRect));
+#else
+#error "Unsupported platform :("
 #endif
 	Poly::gEngine->Init(std::move(game), std::move(device));
 	Poly::gConsole.LogDebug("Engine loaded successfully");
--- PolyEngine/PolyEditor/Src/PolyViewportWidget.hpp
+++ PolyEngine/PolyEditor/Src/PolyViewportWidget.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QWidget>
+#include <QtGui/QtGui>
 
 class PolyViewportWidget : public QWidget
 {
