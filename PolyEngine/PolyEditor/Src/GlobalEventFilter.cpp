26b958e40d4062934c1f73dbf6a66381d0ce0c55
Squares <m.piotr@moscicki.pl>
2017-12-15 08:56:44 +0100

    UNUSED(variable) fix in GlobalEventFilter.

--- PolyEngine/PolyEditor/Src/GlobalEventFilter.cpp
+++ PolyEngine/PolyEditor/Src/GlobalEventFilter.cpp
@@ -2,6 +2,7 @@
 
 bool GlobalEventFilter::eventFilter(QObject* watched, QEvent* event)
 {
+	UNUSED(watched);
 	gApp->DockManager.ProcessEvent(event);
 
 	return false; // you should return false when you want qt to forward this event to receiver
