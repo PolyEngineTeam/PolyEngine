723a8e1fb7aba850ee5fb72b35417e20e5628154
Squares <m.piotr@moscicki.pl>
2017-12-30 23:03:22 +0100

    Added some declarations in Rigidbody3DComponent.

--- PolyEngine/Engine/Src/Rigidbody3DComponent.hpp
+++ PolyEngine/Engine/Src/Rigidbody3DComponent.hpp
@@ -22,18 +22,61 @@ namespace Poly
 		_COUNT
 	};
 
+	struct Rigidbody3DComponentConstructionInfo
+	{
+		float Mass = 0;
+		float Friction = 0;
+
+		Physics3DShape* Shape;
+		eRigidBody3DType Type;
+	};
+
 	class ENGINE_DLLEXPORT Rigidbody3DComponent : public ComponentBase
 	{
 		friend void Physics3DSystem::Physics3DUpdatePhase(World* world);
 	public:
-		Rigidbody3DComponent(World* world, eRigidBody3DType type, Physics3DShape* shape, float mass = 0);
+		Rigidbody3DComponent(World* world, eRigidBody3DType type, Physics3DShape* shape, float mass);
 		~Rigidbody3DComponent();
 
-		eRigidBody3DType GetBodyType() const { return BodyType; }
+		void ApplyForceToCenter(const Vector& force);
+		void ApplyImpulseToCenter(const Vector& impulse);
+		void ApplyDamping(float timestep);
+		void ApplyForce(const Vector& force, const Vector& relPos);
+		void ApplyGravity();
+		void ApplyImpulse(const Vector& impulse, const Vector& relPos);
+		void ApplyTorque(const Vector& torque);
+		void ApplyTorqueImpulse(const Vector& torque);
+
+		void ClearForces();
+
+		void SetAngularFactor();
+		const Vector& GetAngularFactor();
+		void SetAngularVelocity();
+		const Vector& GetAngularVelocity();
+
+		void SetDamping(float dampfactor);
+		float GetLinearDamping();
+		float GetAngularDamping();
+
+		void SetGravity();
+		float GetGravity();
+
+		void SetLinearFactor();
+		const Vector& GetLinearFactor();
+		void SetLinearVelocity();
+		const Vector& GetLinearVelocity();
+
+		void SetMassProperties();
+		float GetInvMass();
+
+		void SetFixedTranslation(bool fixed);
+		void SetFixedRotation(bool fixed);
+
 		void UpdatePosition();
 
-	private:
+		eRigidBody3DType GetBodyType() const { return BodyType; }
 
+	private:
 		World* BodyWorld;
 		eRigidBody3DType BodyType;
 
