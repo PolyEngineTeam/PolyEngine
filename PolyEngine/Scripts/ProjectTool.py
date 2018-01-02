6c263abeb69d0b3c162d6d444a7a54b735ebf204
Michal Majczak <michal.majczak92@gmail.com>
2018-01-02 12:34:28 +0100

    Working arg parsing.

--- PolyEngine/Scripts/ProjectTool.py
+++ PolyEngine/Scripts/ProjectTool.py
@@ -6,7 +6,7 @@ class ActionType(Enum):
     UPDATE = 'Update'
 
     def __str__(self):
-        return self.value
+        return self.name
 
 # Custom action for project creation
 class CreateProjectAction(argparse.Action):
@@ -15,7 +15,9 @@ class CreateProjectAction(argparse.Action):
             raise ValueError("Nargs must be 2 !")
         super(CreateProjectAction, self).__init__(option_strings, dest, nargs, **kwargs)
     def __call__(self, parser, namespace, values, option_string=None):
-        CreateProject(values[0], values[1])
+        setattr(namespace, self.dest, ActionType.CREATE)
+        setattr(namespace, self.metavar[0], values[0])
+        setattr(namespace, self.metavar[1], values[1])
 
 class UpdateProjectAction(argparse.Action):
     def __init__(self, option_strings, dest, nargs, **kwargs):
@@ -23,15 +25,16 @@ class UpdateProjectAction(argparse.Action):
             raise ValueError("Nargs must be 1 !")
         super(UpdateProjectAction, self).__init__(option_strings, dest, nargs, **kwargs)
     def __call__(self, parser, namespace, values, option_string=None):
-        UpdateProject(values[0])
+        setattr(namespace, self.dest, ActionType.UPDATE)
+        setattr(namespace, self.metavar, values[0])
 
 # Functions
 # Create project function
-def CreateProject(name, path):
-    print('test project ', name, ' creation in ', path, ' engine at ', args.enginePath)
+def CreateProject(name, path, enginePath):
+    print('Creating project', name, 'in', path, 'with engine at', args.enginePath)
 
-def UpdateProject(path):
-    print('Updating project at', path, ' engine at ', args.enginePath)
+def UpdateProject(path, enginePath):
+    print('Updating project at', path, 'with engine at', args.enginePath)
 
 #################### SCRIPT START ####################
 parser = argparse.ArgumentParser(description='PolyEngine project management tool')
@@ -42,19 +45,17 @@ parser.add_argument("-e", "--engine", action='store', dest='enginePath',
                     help='provide custom engine path')
 
 mtx = parser.add_mutually_exclusive_group()
-mtx.add_argument('-c', '--create', action='store_const', dest='actionToPerform',
-                    const=ActionType.CREATE, default=None,
+mtx.add_argument('-c', '--create', action=CreateProjectAction, dest='actionToPerform',
                     nargs=2, metavar=('project_name', 'project_path'),
                     help='create project of given name at provided path')
-mtx.add_argument('-u', '--update', action='store_const', dest='actionToPerform',
-                    const=ActionType.UPDATE, default=None,
+mtx.add_argument('-u', '--update', action=UpdateProjectAction, dest='actionToPerform',
                     nargs=1, metavar='project_path',
                     help='update project at given path')
 
 
 args = parser.parse_args()
-print(args.actionToPerform)
-#if(args.actionToPerform == ActionType.CREATE)
- #   CreateProject(args.project_name, args.project_path)
 
-
+if args.actionToPerform == ActionType.CREATE:
+    CreateProject(args.project_name, args.project_path, args.enginePath)
+elif args.actionToPerform == ActionType.UPDATE:
+    UpdateProject(args.project_path, args.enginePath)
