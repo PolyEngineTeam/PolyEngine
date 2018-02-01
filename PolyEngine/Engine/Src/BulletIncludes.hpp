b1ad425fb4ad41e14ac37f6f5a8c97e4562339fa
Squares <m.piotr@moscicki.pl>
2018-02-01 17:45:23 +0100

    No error try.

--- PolyEngine/Engine/Src/BulletIncludes.hpp
+++ PolyEngine/Engine/Src/BulletIncludes.hpp
@@ -2,7 +2,7 @@
 
 #include <Defines.hpp>
 
-SILENCE_GCC_WARNING(-Werror, "Bullet includes has warnings")
+SILENCE_GCC_WARNING(-Wno-error, "Bullet includes has warnings")
 #include <btBulletDynamicsCommon.h>
 #include <btBulletCollisionCommon.h>
 UNSILENCE_GCC_WARNING()
