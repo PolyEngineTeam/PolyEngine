bd3bada98287a44b422cc47f3cdda191ff14d555
Piotr Mościcki <m.piotr@moscicki.pl>
2018-04-23 18:53:18 +0200

    Better tests.

--- PolyEngine/UnitTests/Src/ComponentIterationTests.cpp
+++ PolyEngine/UnitTests/Src/ComponentIterationTests.cpp
@@ -26,94 +26,82 @@ TEST_CASE("World component iteration tests.", "ComponentIterator")
 	DeferredTaskSystem::AddComponentImmediate<FreeFloatMovementComponent>(w, e[1]);
 
 	e[2] = DeferredTaskSystem::SpawnEntityImmediate(w);
+	DeferredTaskSystem::AddComponentImmediate<SoundListenerComponent>(w, e[2]);
 	DeferredTaskSystem::AddComponentImmediate<FreeFloatMovementComponent>(w, e[2]);
-	DeferredTaskSystem::AddComponentImmediate<PostprocessSettingsComponent>(w, e[2]);
 
 	e[3] = DeferredTaskSystem::SpawnEntityImmediate(w);
-	DeferredTaskSystem::AddComponentImmediate<SoundListenerComponent>(w, e[3]);
+	DeferredTaskSystem::AddComponentImmediate<FreeFloatMovementComponent>(w, e[3]);
 	DeferredTaskSystem::AddComponentImmediate<PostprocessSettingsComponent>(w, e[3]);
 
 	e[4] = DeferredTaskSystem::SpawnEntityImmediate(w);
-	DeferredTaskSystem::AddComponentImmediate<SoundListenerComponent>(w, e[4]);
+	DeferredTaskSystem::AddComponentImmediate<FreeFloatMovementComponent>(w, e[4]);
+	DeferredTaskSystem::AddComponentImmediate<PostprocessSettingsComponent>(w, e[4]);
 
 	e[5] = DeferredTaskSystem::SpawnEntityImmediate(w);
 	DeferredTaskSystem::AddComponentImmediate<FreeFloatMovementComponent>(w, e[5]);
+	DeferredTaskSystem::AddComponentImmediate<PostprocessSettingsComponent>(w, e[5]);
 
 	e[6] = DeferredTaskSystem::SpawnEntityImmediate(w);
-	DeferredTaskSystem::AddComponentImmediate<PostprocessSettingsComponent>(w, e[6]);
+	DeferredTaskSystem::AddComponentImmediate<SoundListenerComponent>(w, e[6]);
 
 	e[7] = DeferredTaskSystem::SpawnEntityImmediate(w);
-	DeferredTaskSystem::AddComponentImmediate<FreeFloatMovementComponent>(w, e[7]);
+	DeferredTaskSystem::AddComponentImmediate<SoundListenerComponent>(w, e[7]);
 
 	e[8] = DeferredTaskSystem::SpawnEntityImmediate(w);
-	DeferredTaskSystem::AddComponentImmediate<PostprocessSettingsComponent>(w, e[8]);
+	DeferredTaskSystem::AddComponentImmediate<SoundListenerComponent>(w, e[8]);
 
 	e[9] = DeferredTaskSystem::SpawnEntityImmediate(w);
-	DeferredTaskSystem::AddComponentImmediate<PostprocessSettingsComponent>(w, e[9]);
+	DeferredTaskSystem::AddComponentImmediate<SoundListenerComponent>(w, e[9]);
 
 	//	
-	//	SoundListenerComponent			|	0 1 3 4
-	//	FreeFloatMovementComponent		|	0 1 2 5 7
-	//	PostprocessSettingsComponent	|	0 2 3 6 8 9 
+	//	SoundListenerComponent			|	0 1 2       6 7 8 9
+	//	FreeFloatMovementComponent		|	0 1 2 3 4 5
+	//	PostprocessSettingsComponent	|	0 	  3 4 5
 	//
 
 	int i = 0;
 	for (auto c : w->IterateComponents<SoundListenerComponent>())
 		++i;
-	REQUIRE(i == 4);
+	REQUIRE(i == 7);
 
 	i = 0;
 	for (auto c : w->IterateComponents<FreeFloatMovementComponent>())
 		++i;
-	REQUIRE(i == 5);
+	REQUIRE(i == 6);
 
 	i = 0;
 	for (auto c : w->IterateComponents<PostprocessSettingsComponent>())
 		++i;
-	REQUIRE(i == 6);
+	REQUIRE(i == 4);
 
 	i = 0;
 	for (auto c : w->IterateComponents<SoundListenerComponent, FreeFloatMovementComponent>())
 		++i;
-	REQUIRE(i == 2);
+	REQUIRE(i == 3);
 
 	i = 0;
 	for (auto c : w->IterateComponents<FreeFloatMovementComponent, SoundListenerComponent>())
 		++i;
-	REQUIRE(i == 2);
-
-	i = 0;
-	for (auto c : w->IterateComponents<SoundListenerComponent, PostprocessSettingsComponent>())
-		++i;
-	REQUIRE(i == 2);
-
-	i = 0;
-	for (auto c : w->IterateComponents<PostprocessSettingsComponent, SoundListenerComponent>())
-		++i;
-	REQUIRE(i == 2);
+	REQUIRE(i == 3);
 
 	i = 0;
 	for (auto c : w->IterateComponents<FreeFloatMovementComponent, PostprocessSettingsComponent>())
 		++i;
-	REQUIRE(i == 2);
+	REQUIRE(i == 4);
 
 	i = 0;
 	for (auto c : w->IterateComponents<PostprocessSettingsComponent, FreeFloatMovementComponent>())
 		++i;
-	REQUIRE(i == 2);
+	REQUIRE(i == 4);
 
 	i = 0;
-	for (auto c : w->IterateComponents<SoundListenerComponent, FreeFloatMovementComponent, PostprocessSettingsComponent>())
+	for (auto c : w->IterateComponents<FreeFloatMovementComponent, SoundListenerComponent, PostprocessSettingsComponent>())
 		++i;
 	REQUIRE(i == 1);
 
 	i = 0;
-	for (auto c : w->IterateComponents<FreeFloatMovementComponent, PostprocessSettingsComponent, SoundListenerComponent>())
+	for (auto c : w->IterateComponents<PostprocessSettingsComponent, FreeFloatMovementComponent, SoundListenerComponent>())
 		++i;
 	REQUIRE(i == 1);
 
-	i = 0;
-	for (auto c : w->IterateComponents<PostprocessSettingsComponent, SoundListenerComponent, FreeFloatMovementComponent>())
-		++i;
-	REQUIRE(i == 1);
 }
