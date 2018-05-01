153c896956172f0f98268b85099db9b4ded2d331
Michał Majczak <michal.majczak92@gmail.com>
2018-05-01 14:04:01 +0200

    Implemented map serialization. (#131)
    * Implemented map serialization.
    
    * fix to unit tests code.

--- PolyEngine/Core/Src/RTTI/CustomTypeTraits.hpp
+++ PolyEngine/Core/Src/RTTI/CustomTypeTraits.hpp
@@ -4,6 +4,7 @@
 #include "BaseObject.hpp"
 
 #include "Collections/Dynarray.hpp"
+#include "Collections/OrderedMap.hpp"
 
 namespace Poly
 {
@@ -15,6 +16,13 @@ namespace Poly
 
 		template <typename> struct DynarrayValueType {};
 		template <typename T> struct DynarrayValueType<Dynarray<T>> { using type = T; };
+
+		// Is Ordered map
+		template <typename> struct IsOrderedMap : public std::false_type {};
+		template <typename K, typename V> struct IsOrderedMap<OrderedMap<K,V>> : public std::true_type {};
+
+		template <typename> struct OrderedMapType {};
+		template <typename K, typename V> struct OrderedMapType<OrderedMap<K, V>> { using keyType = K; using valueType = V; };
 
 		template <class T> using RawType = std::remove_pointer<typename std::decay<typename std::remove_cv<T>::type >::type>;
 	}
