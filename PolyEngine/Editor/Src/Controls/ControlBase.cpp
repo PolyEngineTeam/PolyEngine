979550b2b3417efced6a26fbd0957f629a4bafb7
Piotr Mościcki <m.piotr@moscicki.pl>
2018-05-14 22:52:33 +0200

    ControlBase comments.

--- PolyEngine/Editor/Src/Controls/ControlBase.cpp
+++ PolyEngine/Editor/Src/Controls/ControlBase.cpp
@@ -4,10 +4,12 @@ namespace Impl
 {
 	ControlCreator* CoreTypeToControlMap = 0;
 
+	//------------------------------------------------------------------------------
 	void* ControlCreator::operator new(size_t, void* where)
 	{
 		static bool mapInitialized = false;
 
+		// if cntrol creator is created for the firt time, memory must be allocated for CoreTypeToControlMap
 		if (!mapInitialized)
 		{
 			mapInitialized = true;
@@ -25,6 +27,7 @@ namespace Impl
 	}
 }
 
+//------------------------------------------------------------------------------
 void ControlBase::SetObject(void* ptr, const RTTI::Property* prop)
 {
 	Object = ptr; 
--- PolyEngine/Editor/Src/Controls/ControlBase.hpp
+++ PolyEngine/Editor/Src/Controls/ControlBase.hpp
@@ -29,27 +29,48 @@ namespace Impl
 	extern ControlCreator* CoreTypeToControlMap;
 }
 
+// This is base class for all controls for core types such as int, string or vector.
+// @see Poly::RTTI::eCorePropertyType
 class ControlBase : public QWidget
 {
 public:
 	ControlBase() = default;
 
+	// Sets object assigned to control ans updates this control.
+	// @see ControlBase::UpdateControl;
 	void SetObject(void* ptr, const RTTI::Property* prop);
+
+	// Call this to update assigned object from current control state.
+	// If ControlBase::ASAPUpdate is set to true probably You will never need to use this fnction.
 	virtual void UpdateObject() = 0;
+
+	// Call this to update control state from assigned object.
+	// Unlike ControlBase::UpdateObject You have to call this function by yourself.
+	// Besides this function is getting called every time you call ControlBase::SetObject.
+	// @see ControlBase::SetObject;
 	virtual void UpdateControl() = 0;
 
+	// Returns ptr to newly created proper control for given core type.
 	static ControlBase* CreateControl(RTTI::eCorePropertyType type) 
 	{ 
 		return ::Impl::CoreTypeToControlMap[static_cast<int>(type)]();
 	}
 
+	// if You want this control to update as soon as state is changed, enter pressed or 
+	// focus lost set this to true. Otherwise You will have to update it calling UpdateObject.
+	// @see ControlBase::UpdateObject
 	bool ASAPUpdate = true;
 
 protected:
+	// Pointer to an object which is assigned to this control.
 	void* Object = nullptr;
+
+	// Property of assigned object. Useful when one control is assigned to multiple core types
+	// like NumberControl.
 	const RTTI::Property* Property;
 
 public slots:
+	// Use this as slot to connect to Your fields' signals like editingFinished in QLineEdit.
 	void Confirm()
 	{
 		if (ASAPUpdate)
@@ -57,6 +78,7 @@ public slots:
 	}
 };
 
+// Use this to add Your control to map from core type to control creator function.
 #define ASSIGN_CONTROL(CONTROL, CORE_TYPE) \
 	namespace Impl \
 	{ \
