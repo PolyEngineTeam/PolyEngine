a74f4289499e5fcbde91c1ec2cbddf136914784f
Michał Kłoś <sp0lsh@users.noreply.github.com>
2018-06-26 23:52:05 +0200

    removed comments

--- PolyEngine/Engine/Res/Shaders/hdr.frag.glsl
+++ PolyEngine/Engine/Res/Shaders/hdr.frag.glsl
@@ -17,11 +17,7 @@ out vec4 fragColor;
 void main()
 {
     vec3 color = texture(hdrBuffer, vUV).rgb;
-    // vec3 color = texelFetch(hdrBuffer, vUV, 0).rgb;
     vec3 result = vec3(1.0) - exp(-color * uExposure);
 
-	// Minor gamma correction. Need to expand on it
-    // const float gamma = 2.2;
-    // result = pow(result, vec3(1.0 / gamma));
     fragColor = vec4(result, 1.0);
 }
