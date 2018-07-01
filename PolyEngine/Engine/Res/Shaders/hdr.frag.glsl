44fd200728a0065ffa6119b794c492c42be0cdd5
Michal Kłos <michal.m.klos@gmail.com>
2018-07-01 19:11:51 +0200

    review fixes

--- PolyEngine/Engine/Res/Shaders/hdr.frag.glsl
+++ PolyEngine/Engine/Res/Shaders/hdr.frag.glsl
@@ -2,22 +2,19 @@
 
 in vec2 vUV;
 
-// Uniforms
-// Texture for the hdr buffer
-uniform sampler2D hdrBuffer;
+uniform sampler2D uHdrBuffer;
 
-// Controls exposure level of image
 uniform float uExposure;
 
 out vec4 fragColor;
 
 // Uses Reinhard tonemapping https://www.cs.utah.edu/~reinhard/cdrom/tonemap.pdf
 // with an added controllable exposure component
-
 void main()
 {
-    vec3 color = texture(hdrBuffer, vUV).rgb;
+    vec3 color = texture(uHdrBuffer, vUV).rgb;
     vec3 result = vec3(1.0) - exp(-color * uExposure);
+    // vec3 result = vec3(1.0) - exp(-color * 2.0);
 
     fragColor = vec4(result, 1.0);
 }
