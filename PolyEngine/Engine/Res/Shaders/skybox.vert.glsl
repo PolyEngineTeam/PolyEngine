8cfc98e87fe6d764f857fbb68f9de17536f6464b
Michal Kłos <michal.m.klos@gmail.com>
2018-07-01 19:26:53 +0200

    review fix

--- PolyEngine/Engine/Res/Shaders/skybox.vert.glsl
+++ PolyEngine/Engine/Res/Shaders/skybox.vert.glsl
@@ -8,9 +8,7 @@ uniform mat4 uClipFromWorld;
 
 void main()
 {
-    // vec3 textureFlipXFix = vec3(-1.0, 1.0, 1.0);
-	// vUV = normalize(aPositionInModel * textureFlipXFix);
-    vUV = normalize(aPositionInModel);
-    vec4 pos = uClipFromWorld * vec4(aPositionInModel, 1.0f);
+	vUV = normalize(aPositionInModel);
+	vec4 pos = uClipFromWorld * vec4(aPositionInModel, 1.0f);
 	gl_Position = pos.xyww;
 }
