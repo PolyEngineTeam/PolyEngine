37a6b80f05c7342741cdd46aa3697f60b94902b4
Michal Kłos <michal.m.klos@gmail.com>
2018-07-01 19:26:14 +0200

    review fix

--- PolyEngine/Engine/Res/Shaders/linearizeDepth.frag.glsl
+++ PolyEngine/Engine/Res/Shaders/linearizeDepth.frag.glsl
@@ -9,14 +9,11 @@ uniform float uFar;
 out vec4 oColor;
 
 float LinearizeDepth(float depth) {
-	// float z = depth * 2.0 - 1.0;	
-	// return (2.0 * uNear * uFar) / (uFar + uNear - z * (uFar - uNear));
 	return -uFar * uNear / (depth * (uFar - uNear) - uFar);
 }
 
 void main()
 {
     float depth = texture(uDepth, vUV).r;
-    // oColor = vec4(LinearizeDepth(depth), 0.0, 0.0, 1.0);	
 	oColor = vec4(depth, LinearizeDepth(depth)/uFar, 0.0, 1.0 );
 }
--- PolyEngine/Engine/Res/Shaders/skybox.frag.glsl
+++ PolyEngine/Engine/Res/Shaders/skybox.frag.glsl
@@ -10,12 +10,6 @@ layout(location = 1) out vec4 normal;
 
 void main()
 {
-    // float mipLevel = 5.0 * fract(0.1 * uTime);
-    // vec3 envColor = texture(uCubemap, vUV, mipLevel - fract(mipLevel)).rgb;
-    vec3 envColor = texture(uCubemap, vUV).rgb;
-    // envColor = envColor / (envColor / vec3(1.0));    // HDR -> LDR
-    // envColor = pow(envColor, vec3(1.0, 2.2));        // linear -> gamma
-
-    color = vec4(envColor, 1.0);
-    normal = vec4(0.0);
+	color = vec4(texture(uCubemap, vUV).rgb, 1.0);
+	normal = vec4(0.0);
 }
