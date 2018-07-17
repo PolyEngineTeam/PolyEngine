ef6ac04029f63e10c3fc2a680397f6755de35b6e
Michal 'spolsh' Kłoś <michal.m.klos@gmail.com>
2018-07-17 21:01:47 +0200

    branchless selection of bright pixels

--- PolyEngine/Engine/Res/Shaders/bloom_pass_bright.frag.glsl
+++ PolyEngine/Engine/Res/Shaders/bloom_pass_bright.frag.glsl
@@ -9,11 +9,7 @@ out vec4 oColor;
 void main()
 {
     vec3 input = texture(uImage, vUV).rgb;
-    // check whether fragment output is higher than threshold, if so output as brightness color
     float luminance = dot(input, vec3(0.2126, 0.7152, 0.0722));
-    vec4 BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
-    if (luminance > 1.0)
-        BrightColor = vec4(input, 1.0);
-
+    vec4 BrightColor = mix(vec4(0.0, 0.0, 0.0, 1.0), vec4(input, 1.0), step(1.0, luminance));
     oColor = BrightColor;
 }
