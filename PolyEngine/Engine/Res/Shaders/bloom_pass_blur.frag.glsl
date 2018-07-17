629a41233b8a18022819b447a71834a9084a5b40
Michal 'spolsh' Kłoś <michal.m.klos@gmail.com>
2018-07-17 21:23:37 +0200

    branchless blur direction

--- PolyEngine/Engine/Res/Shaders/bloom_pass_blur.frag.glsl
+++ PolyEngine/Engine/Res/Shaders/bloom_pass_blur.frag.glsl
@@ -3,7 +3,7 @@
 in vec2 vUV;
 
 uniform sampler2D uImage;
-uniform int uHorizontal;
+uniform float uIsHorizontal;
 
 uniform float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
 
@@ -13,49 +13,11 @@ void main()
 {
     vec2 tex_offset = 1.0 / textureSize(uImage, 0); // gets size of single texel
     vec3 result = texture(uImage, vUV).rgb * weight[0]; // current fragment's contribution
-    if(uHorizontal > 0)
+    vec2 blurDir = mix(vec2(1.0, 0.0), vec2(0.0, 1.0), step(0.5, uIsHorizontal));
+    for(int i = 1; i < 5; ++i)
     {
-        for(int i = 1; i < 5; ++i)
-        {
-            result += texture(uImage, vUV + vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
-            result += texture(uImage, vUV - vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
-        }
-    }
-    else
-    {
-        for(int i = 1; i < 5; ++i)
-        {
-            result += texture(uImage, vUV + vec2(0.0, tex_offset.y * i)).rgb * weight[i];
-            result += texture(uImage, vUV - vec2(0.0, tex_offset.y * i)).rgb * weight[i];
-        }
+        result += texture(uImage, vUV + blurDir * tex_offset.x * i).rgb * weight[i];
+        result += texture(uImage, vUV - blurDir * tex_offset.x * i).rgb * weight[i];
     }
     oColor = vec4(result, 1.0);
 }
-
-//// 
-
-// uniform bool horizontal;
-/*
-void main()
-{             
-    vec2 tex_offset = 1.0 / textureSize(image, 0); // gets size of single texel
-    vec3 result = texture(image, TexCoords).rgb * weight[0]; // current fragment's contribution
-    if(horizontal)
-    {
-        for(int i = 1; i < 5; ++i)
-        {
-            result += texture(image, TexCoords + vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
-            result += texture(image, TexCoords - vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
-        }
-    }
-    else
-    {
-        for(int i = 1; i < 5; ++i)
-        {
-            result += texture(image, TexCoords + vec2(0.0, tex_offset.y * i)).rgb * weight[i];
-            result += texture(image, TexCoords - vec2(0.0, tex_offset.y * i)).rgb * weight[i];
-        }
-    }
-    FragColor = vec4(result, 1.0);
-}
-*/
