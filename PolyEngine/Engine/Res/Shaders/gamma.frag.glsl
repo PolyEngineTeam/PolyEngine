eb1eea2cfbed2aad946ceb280ba15c192cf04d6c
Michal Kłos <michal.m.klos@gmail.com>
2018-07-22 11:11:25 +0200

    updated splash, color temp tint

--- PolyEngine/Engine/Res/Shaders/gamma.frag.glsl
+++ PolyEngine/Engine/Res/Shaders/gamma.frag.glsl
@@ -16,6 +16,96 @@ uniform float uGamma;
 
 out vec4 oColor;
 
+// playing with this value tweaks how dim or bright the resulting image is
+#define LUMINANCE_PRESERVATION 0.75
+
+#define EPSILON 1e-10
+
+float saturate(float v)
+{
+    return clamp(v, 0.0, 1.0);
+}
+vec2 saturate(vec2 v)
+{
+    return clamp(v, vec2(0.0), vec2(1.0));
+}
+vec3 saturate(vec3 v)
+{
+    return clamp(v, vec3(0.0), vec3(1.0));
+}
+vec4 saturate(vec4 v)
+{
+    return clamp(v, vec4(0.0), vec4(1.0));
+}
+
+vec3 ColorTemperatureToRGB(float temperatureInKelvins)
+{
+    vec3 retColor;
+	
+    temperatureInKelvins = clamp(temperatureInKelvins, 1000.0, 40000.0) / 100.0;
+    
+    if (temperatureInKelvins <= 66.0)
+    {
+        retColor.r = 1.0;
+        retColor.g = saturate(0.39008157876901960784 * log(temperatureInKelvins) - 0.63184144378862745098);
+    }
+    else
+    {
+        float t = temperatureInKelvins - 60.0;
+        retColor.r = saturate(1.29293618606274509804 * pow(t, -0.1332047592));
+        retColor.g = saturate(1.12989086089529411765 * pow(t, -0.0755148492));
+    }
+    
+    if (temperatureInKelvins >= 66.0)
+        retColor.b = 1.0;
+    else if (temperatureInKelvins <= 19.0)
+        retColor.b = 0.0;
+    else
+        retColor.b = saturate(0.54320678911019607843 * log(temperatureInKelvins - 10.0) - 1.19625408914);
+
+    return retColor;
+}
+
+float Luminance(vec3 color)
+{
+    float fmin = min(min(color.r, color.g), color.b);
+    float fmax = max(max(color.r, color.g), color.b);
+    return (fmax + fmin) / 2.0;
+}
+
+vec3 HUEtoRGB(float H)
+{
+    float R = abs(H * 6.0 - 3.0) - 1.0;
+    float G = 2.0 - abs(H * 6.0 - 2.0);
+    float B = 2.0 - abs(H * 6.0 - 4.0);
+    return saturate(vec3(R, G, B));
+}
+
+vec3 HSLtoRGB(in vec3 HSL)
+{
+    vec3 RGB = HUEtoRGB(HSL.x);
+    float C = (1.0 - abs(2.0 * HSL.z - 1.0)) * HSL.y;
+    return (RGB - 0.5) * C + vec3(HSL.z);
+}
+ 
+vec3 RGBtoHCV(vec3 RGB)
+{
+    // Based on work by Sam Hocevar and Emil Persson
+    vec4 P = (RGB.g < RGB.b) ? vec4(RGB.bg, -1.0, 2.0 / 3.0) : vec4(RGB.gb, 0.0, -1.0 / 3.0);
+    vec4 Q = (RGB.r < P.x) ? vec4(P.xyw, RGB.r) : vec4(RGB.r, P.yzx);
+    float C = Q.x - min(Q.w, Q.y);
+    float H = abs((Q.w - Q.y) / (6.0 * C + EPSILON) + Q.z);
+    return vec3(H, C, Q.x);
+}
+
+vec3 RGBtoHSL(vec3 RGB)
+{
+    vec3 HCV = RGBtoHCV(RGB);
+    float L = HCV.z - HCV.y * 0.5;
+    float S = HCV.y / (1.0 - abs(L * 2.0 - 1.0) + EPSILON);
+    return vec3(HCV.x, S, L);
+}
+
 float hash(float c)
 {
     return fract(sin(dot(c, 12.9898)) * 43758.5453);
@@ -70,5 +160,11 @@ void main()
 
     // result.rgb = vec3(splash.a);
     result = mix(result.rgb, uSplashTint.rgb, splash.r * maskX * maskY);
-    oColor = vec4(result, 1.0);
+
+    vec3 blended = result.rgb * ColorTemperatureToRGB(7500.0);
+    vec3 resultHSL = RGBtoHSL(blended);
+    float originalLuminance = Luminance(result.rgb);
+    vec3 luminancePreservedRGB = HSLtoRGB(vec3(resultHSL.x, resultHSL.y, originalLuminance));
+
+    oColor = vec4(luminancePreservedRGB, 1.0);
 }
