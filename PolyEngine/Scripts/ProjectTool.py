eecdb5e683d66414aef27aac2d33b2265affad4f
rafal.klusek <rafal.klusek@websensa.com>
2018-08-09 13:40:14 +0200

    ProjectTool refactor, -c checks out latest version

--- PolyEngine/Scripts/ProjectTool.py
+++ PolyEngine/Scripts/ProjectTool.py
@@ -3,8 +3,10 @@ import os
 import shutil
 import fileinput
 import json
+import subprocess
 from enum import Enum
 import xml.etree.ElementTree as ET
+
 
 class ActionType(Enum):
     CREATE = 'Create',
@@ -13,22 +15,26 @@ class ActionType(Enum):
     def __str__(self):
         return self.name
 
+
 # Custom action for project creation
 class CreateProjectAction(argparse.Action):
     def __init__(self, option_strings, dest, nargs, **kwargs):
         if nargs is not 2:
             raise ValueError("Nargs must be 2 !")
         super(CreateProjectAction, self).__init__(option_strings, dest, nargs, **kwargs)
+
     def __call__(self, parser, namespace, values, option_string=None):
         setattr(namespace, self.dest, ActionType.CREATE)
         setattr(namespace, self.metavar[0], values[0])
         setattr(namespace, self.metavar[1], values[1])
+
 
 class UpdateProjectAction(argparse.Action):
     def __init__(self, option_strings, dest, nargs, **kwargs):
         if nargs is not 1:
             raise ValueError("Nargs must be 1 !")
         super(UpdateProjectAction, self).__init__(option_strings, dest, nargs, **kwargs)
+
     def __call__(self, parser, namespace, values, option_string=None):
         setattr(namespace, self.dest, ActionType.UPDATE)
         setattr(namespace, self.metavar, values[0])
@@ -42,8 +48,10 @@ class UpdateProjectAction(argparse.Action):
 # $GAME_CLASS_NAME$ - name of the game class
 # $ENGINE_DIR$ - absolute path to engine root directory
 
+
 def get_cmake_path(path):
     return path.replace('\\', '/')
+
 
 def replace_tags_in_file(file_to_search, tags_and_values):
     print('Replacing tags in', file_to_search)
@@ -55,6 +63,7 @@ def replace_tags_in_file(file_to_search, tags_and_values):
             for (tag, val) in tags_and_values:
                 line = line.replace(tag, val)
             print(line, end='')
+
 
 def xml_indent(elem, level=0):
     i = "\n" + level*"  "
@@ -71,16 +80,17 @@ def xml_indent(elem, level=0):
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
 
+
 def patch_usr_proj(path, proj_name):
     usr_proj_path = os.sep.join([path, 'Build', proj_name, proj_name + '.vcxproj.user'])
     xml_namespace = { 'ns' : 'http://schemas.microsoft.com/developer/msbuild/2003' }
     namespace_str = '{' + xml_namespace['ns'] + '}'
     ET.register_namespace('', xml_namespace['ns'])
     
-    property_group_conditions_to_add = [ "'$(Configuration)|$(Platform)'=='Debug|x64'",
-                                  "'$(Configuration)|$(Platform)'=='DebugFast|x64'",
-                                  "'$(Configuration)|$(Platform)'=='Release|x64'",
-                                  "'$(Configuration)|$(Platform)'=='Testing|x64'" ]
+    property_group_conditions_to_add = ["'$(Configuration)|$(Platform)'=='Debug|x64'",
+                                        "'$(Configuration)|$(Platform)'=='DebugFast|x64'",
+                                        "'$(Configuration)|$(Platform)'=='Release|x64'",
+                                        "'$(Configuration)|$(Platform)'=='Testing|x64'"]
 
     if os.path.exists(usr_proj_path):
         # Load file if exists
@@ -110,19 +120,19 @@ def patch_usr_proj(path, proj_name):
         print('Patching property group:', project_property_group.attrib['Condition'])
         
         dbg_cmd = project_property_group.find('ns:LocalDebuggerCommand', xml_namespace)
-        if dbg_cmd == None:
+        if dbg_cmd is None:
             dbg_cmd = ET.SubElement(project_property_group, namespace_str + 'LocalDebuggerCommand')
         dbg_cmd.text = 'PolyStandalone.exe'
         print('\tset LocalDebuggerCommand to', dbg_cmd.text)
         
         dbg_work_dir = project_property_group.find('ns:LocalDebuggerWorkingDirectory', xml_namespace)
-        if dbg_work_dir == None:
+        if dbg_work_dir is None:
             dbg_work_dir = ET.SubElement(project_property_group, namespace_str + 'LocalDebuggerWorkingDirectory')
         dbg_work_dir.text = '$(OutputPath)'
         print('\tset LocalDebuggerWorkingDirectory to', dbg_work_dir.text)
 
         dbg_flavor = project_property_group.find('ns:DebuggerFlavor', xml_namespace)
-        if dbg_flavor == None:
+        if dbg_flavor is None:
             dbg_flavor = ET.SubElement(project_property_group, namespace_str + 'DebuggerFlavor')
         dbg_flavor.text = 'WindowsLocalDebugger'
         print('\tset DebuggerFlavor to', dbg_flavor.text)
@@ -130,12 +140,14 @@ def patch_usr_proj(path, proj_name):
     xml_indent(root)
     xml_data.write(usr_proj_path, encoding='utf-8', xml_declaration=True)
 
+
 def run_cmake(path, build_dir_name, proj_name):
     build_dir_path = os.sep.join([path, build_dir_name])
     if not os.path.exists(build_dir_path):
         os.makedirs(build_dir_path)
     
-    # Run cmake update with 64bit arch (using undocumented parameters that work for some reason, src: http://cprieto.com/posts/2016/10/cmake-out-of-source-build.html)
+    # Run cmake update with 64bit arch (using undocumented parameters that work for some reason,
+    # src: http://cprieto.com/posts/2016/10/cmake-out-of-source-build.html)
     if os.name == 'nt':
         os.system('cmake -A x64 -H{} -B{}'.format(get_cmake_path(path), get_cmake_path(build_dir_path)))
     else:
@@ -145,11 +157,15 @@ def run_cmake(path, build_dir_name, proj_name):
     if os.name == 'nt':
         patch_usr_proj(path, proj_name)
 
-def create_project_file(path, proj_name):
-    data = {}
-    data['ProjectName'] = proj_name
+
+def create_project_file(path, proj_name, tag=None):
+    data = {
+        'ProjectName': proj_name,
+        'EngineVersion': tag
+    }
     with open(os.sep.join([path, proj_name + '.proj.json']), 'w') as outfile:
         json.dump(data, outfile)
+
 
 def read_project_file(path):
     for file in os.listdir(path):
@@ -157,6 +173,7 @@ def read_project_file(path):
             with open(os.sep.join([path, file])) as json_file:  
                     data = json.load(json_file)
             return data
+
 
 def update_config_files(path, name, engine_path, is_new_config):
     project_path = os.sep.join([path, name])
@@ -178,33 +195,40 @@ def update_config_files(path, name, engine_path, is_new_config):
         replace_tags_in_file(game_hpp_output_path, [('$GAME_CLASS_NAME$', 'Game')])
         replace_tags_in_file(game_cpp_output_path, [('$GAME_CLASS_NAME$', 'Game')])
     # Cmake needs paths with '/' (instead of '\') regardles of platform
-    replace_tags_in_file(cmake_sln_output_path, [('$PROJECT_PATH$', name), ('$PROJECT_NAME$', name), ('$ENGINE_DIR$', get_cmake_path(os.path.abspath(engine_path)))]) 
+    cmake_sln_output_path_tuples = [
+        ('$PROJECT_PATH$', name),
+        ('$PROJECT_NAME$', name),
+        ('$ENGINE_DIR$', get_cmake_path(os.path.abspath(engine_path)))
+    ]
+    replace_tags_in_file(cmake_sln_output_path, cmake_sln_output_path_tuples)
     replace_tags_in_file(cmake_proj_output_path, [('$PROJECT_NAME$', name)])
 
     create_fast_update_script(engine_path, path)
 
+
 def create_fast_update_script(engine_path, proj_path):
     abs_engine_path = os.path.abspath(engine_path)
     script_path = os.sep.join([abs_engine_path, 'Scripts', 'ProjectTool.py'])
-   
-    fileString = 'py {} -e {} -u {}'.format(script_path, abs_engine_path, proj_path)
     
     if os.name == 'nt':
-        fileString = 'py {} -e {} -u {}'.format(script_path, abs_engine_path, proj_path)
-        fileName = os.sep.join([proj_path, 'quick_update.bat'])
+        file_content = 'py {} -e {} -u {}'.format(script_path, abs_engine_path, proj_path)
+        file_name = os.sep.join([proj_path, 'quick_update.bat'])
     else:
-        fileString = '#!/bin/bash\npython3.6 {} -e {} -u {}'.format(script_path, abs_engine_path, proj_path)
-        fileName = os.sep.join([proj_path, 'quick_update.sh'])
+        file_content = '#!/bin/bash\npython3.6 {} -e {} -u {}'.format(script_path, abs_engine_path, proj_path)
+        file_name = os.sep.join([proj_path, 'quick_update.sh'])
 
-    with open(fileName, 'w') as outfile:
-        outfile.write(fileString)
+    with open(file_name, 'w') as outfile:
+        outfile.write(file_content)
 
     if os.name == 'posix':
-        os.system("chmod +x {}".format(fileName))
+        os.system("chmod +x {}".format(file_name))
         
 
 def create_project(name, path, engine_path):
     print('Creating project', name, 'in', path, 'with engine at', engine_path)
+
+    tag = get_latest_tag(engine_path)
+    checkout(engine_path, tag)
 
     if os.path.exists(path):
         raise Exception('Path', path, 'already exists. Cannot create project there!')
@@ -222,8 +246,9 @@ def create_project(name, path, engine_path):
 
     update_config_files(path, name, engine_path, True)
 
-    create_project_file(path, name)
+    create_project_file(path, name, tag)
     run_cmake(path, 'Build', name)
+
 
 def update_project(path, engine_path):
     print('Updating project at', path, 'with engine at', engine_path)
@@ -238,7 +263,20 @@ def update_project(path, engine_path):
 
     run_cmake(path, 'Build', name)
 
-#################### SCRIPT START ####################
+
+def get_latest_tag(engine_path):
+    checkout(engine_path, 'dev')
+    tag_raw = subprocess.check_output('cd {} && git describe --tags --abbrev=0'.format(engine_path), shell=True)
+    tag = tag_raw.decode('utf-8').strip()
+    print('Latest engine version: {}'.format(tag))
+    return tag
+
+
+def checkout(engine_path, identifier):
+    print('Checking out engine from branch/tag: {}'.format(identifier))
+    os.system('cd {} && git stash && git checkout {} && git fetch --tags'.format(engine_path, identifier))
+
+
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(description='PolyEngine project management tool')
 
@@ -249,12 +287,11 @@ if __name__ == "__main__":
 
     MTX = PARSER.add_mutually_exclusive_group()
     MTX.add_argument('-c', '--create', action=CreateProjectAction, dest='action_to_perform',
-                        nargs=2, metavar=('project_path', 'project_name'),
-                        help='create project of given name at provided path')
+                     nargs=2, metavar=('project_path', 'project_name'),
+                     help='create project of given name at provided path')
     MTX.add_argument('-u', '--update', action=UpdateProjectAction, dest='action_to_perform',
-                        nargs=1, metavar='project_path',
-                        help='update project at given path')
-
+                     nargs=1, metavar='project_path',
+                     help='update project at given path')
 
     ARGS = PARSER.parse_args()
 
