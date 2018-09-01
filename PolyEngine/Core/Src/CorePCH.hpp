ca5df128b71e4476bf9f99bf8b24b5ebc7b10e37
Jakub Duchniewicz\jduchniewicz <j.duchniewicz@gmail.com>
2018-09-01 11:49:04 +0200

    app-veyor fix

--- PolyEngine/Core/Src/CorePCH.hpp
+++ PolyEngine/Core/Src/CorePCH.hpp
@@ -3,7 +3,7 @@
 #include "Defines.hpp"
 
 // rapidjson
-SILENCE_GCC_WARNING(-Wclass-memaccess)
+SILENCE_GCC_WARNING(-Wclass-memaccess, "Rapidjson has no release containing fix to this issue (01.09.18).")
 #include <rapidjson/stringbuffer.h>
 #include <rapidjson/prettywriter.h>
 #include <rapidjson/document.h>
