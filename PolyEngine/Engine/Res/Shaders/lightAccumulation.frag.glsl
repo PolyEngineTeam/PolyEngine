6ad1b39d4af21e13f84d1385d327939c93901ae6
Michal Kłos <michal.m.klos@gmail.com>
2018-09-03 20:48:44 +0200

    percentage closer filtering

--- PolyEngine/Engine/Res/Shaders/lightAccumulation.frag.glsl
+++ PolyEngine/Engine/Res/Shaders/lightAccumulation.frag.glsl
@@ -146,9 +146,19 @@ float calcShadow(vec4 fragPosInDirLight, float NdotL)
     // get depth of current fragment from light's perspective
     float currentDepth = projCoords.z;    
     // check whether current frag pos is in shadow
-	// float bias = max(0.05 * (1.0 - NdotL), 0.005);
-	float bias = 0.005;
-    float shadow = currentDepth - bias > closestDepth ? 0.0 : 1.0;
+	float bias = max(0.05 * (1.0 - NdotL), 0.005);
+	
+    float shadow = 0.0;
+    vec2 texelSize = 1.0 / textureSize(uDirShadowMap, 0);
+    for (int x = -1; x <= 1; ++x)
+    {
+        for (int y = -1; y <= 1; ++y)
+        {
+            float pcfDepth = texture(uDirShadowMap, projCoords.xy + vec2(x, y) * texelSize).r;
+            shadow += currentDepth - bias > pcfDepth ? 0.0 : 1.0;
+        }
+    }
+    shadow /= 9.0;
     
 
     return shadow;
