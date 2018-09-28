ef0e78923b67a59ab9b6a55c3a4b766860fb3320
Michal Kłos <michal.m.klos@gmail.com>
2018-09-28 22:32:17 +0200

    commented #pragma failing travis ci

--- PolyEngine/Engine/Src/Imgui/ImguiSystem.cpp
+++ PolyEngine/Engine/Src/Imgui/ImguiSystem.cpp
@@ -34,7 +34,7 @@ void ImguiSystem::ImguiUpdatePhase(Scene* scene)
 		return;
 	}
 
-#pragma region Imgui ProcessEvent
+// #pragma region Imgui ProcessEvent
 	if (inputCmp->GetWheelPosDelta().X > 0) io.MouseWheelH += 1;
 	if (inputCmp->GetWheelPosDelta().X < 0) io.MouseWheelH -= 1;
 	if (inputCmp->GetWheelPosDelta().Y > 0) io.MouseWheel += 1;
@@ -54,9 +54,9 @@ void ImguiSystem::ImguiUpdatePhase(Scene* scene)
 			io.KeySuper = inputCmp->IsPressed(eKey::LGUI) || inputCmp->IsPressed(eKey::RGUI);
 		}
 	}
-#pragma endregion
+// #pragma endregion
 		
-#pragma	region UpdateMousePosAndButtons
+// #pragma	region UpdateMousePosAndButtons
 
 	// Set OS mouse position if requested (rarely used, only when ImGuiConfigFlags_NavEnableSetMousePos is enabled by user)
 	// if (io.WantSetMousePos)
@@ -96,9 +96,9 @@ void ImguiSystem::ImguiUpdatePhase(Scene* scene)
 // #endif
 // }
 
-#pragma endregion
+// #pragma endregion
 
-#pragma region UpdateMouseCursor
+// #pragma region UpdateMouseCursor
 	if (io.ConfigFlags & ImGuiConfigFlags_NoMouseCursorChange)
 		return;
 
@@ -115,9 +115,9 @@ void ImguiSystem::ImguiUpdatePhase(Scene* scene)
 	// 	SDL_SetCursor(g_MouseCursors[imgui_cursor] ? g_MouseCursors[imgui_cursor] : g_MouseCursors[ImGuiMouseCursor_Arrow]);
 	// 	SDL_ShowCursor(SDL_TRUE);
 	// }
-#pragma endregion
+// #pragma endregion
 
-#pragma region Imgui NewFrame
+// #pragma region Imgui NewFrame
 	IM_ASSERT(io.Fonts->IsBuilt());     // Font atlas needs to be built, call renderer _NewFrame() function e.g. ImGui_ImplOpenGL3_NewFrame()
 										// Setup display size (every frame to accommodate for window resizing)
 	int w, h;
@@ -140,7 +140,7 @@ void ImguiSystem::ImguiUpdatePhase(Scene* scene)
 	// io.DeltaTime = g_Time > 0 ? (float)((double)(current_time - g_Time) / frequency) : (float)(1.0f / 60.0f);
 	io.DeltaTime = deltaTime;
 	// g_Time = time;
-#pragma endregion
+// #pragma endregion
 
 	static bool show_demo_window = true;
 	// static bool show_another_window = false;
