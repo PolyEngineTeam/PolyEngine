a121570cf52730bf14d6f9760b44f9d0a157be00
Michal Kłos <michal.m.klos@gmail.com>
2018-10-21 21:36:51 +0200

    interpolated Percentage Closer Filtering

--- PolyEngine/RenderingDevice/OpenGL/Src/TiledForwardRenderer.hpp
+++ PolyEngine/RenderingDevice/OpenGL/Src/TiledForwardRenderer.hpp
@@ -72,7 +72,7 @@ namespace Poly {
 		const int MAX_NUM_LIGHTS = 1024;
 		const int MAX_LIGHT_COUNT_DIRECTIONAL = 8;
 
-		const unsigned int SHADOWMAP_SIZE = 512;
+		const unsigned int SHADOWMAP_SIZE = 2048;
 
 		// X and Y work group dimension variables for compute shader
 		GLuint WorkGroupsX = 0;
