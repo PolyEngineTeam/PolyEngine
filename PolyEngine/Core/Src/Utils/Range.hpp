301b5d3985a38a664b11bc26264925e6b6bb4469
Michał Majczak <michal.majczak92@gmail.com>
2019-04-25 11:02:05 +0200

    Codding style fixes

--- PolyEngine/Core/Src/Utils/Range.hpp
+++ PolyEngine/Core/Src/Utils/Range.hpp
@@ -3,37 +3,36 @@
 #include <Defines.hpp>
 
 namespace Poly {
-
-   template <typename IT>
-	class Range final : public BaseObjectLiteralType<>
+    template <typename IT>
+    class Range final : public BaseObjectLiteralType<>
 	{
 	public:
-      using Iterator_t = IT;
+        using Iterator_t = IT;
 
-      Range(Iterator_t begin, Iterator_t end)
-      : BeginIt(std::move(begin)), EndIt(std::move(end))
-      {}
+        Range(Iterator_t begin, Iterator_t end)
+            : BeginIt(std::move(begin)), EndIt(std::move(end))
+        {}
 
-      template <typename C>
-      Range(C& collection)
-      : Range(begin(collection), end(collection))
-      {}
+        template <typename C>
+        Range(C& collection)
+            : Range(begin(collection), end(collection))
+        {}
 
-      template <typename C>
-      Range(const C& collection)
-      : Range(cbegin(collection), cend(collection))
-      {}
+        template <typename C>
+        Range(const C& collection)
+            : Range(cbegin(collection), cend(collection))
+        {}
 
-		Iterator_t Begin() { return BeginIt; }
-      Iterator_t End() { return EndIt; }
-   private:
-      Iterator_t BeginIt;
-      Iterator_t EndIt;
+        Iterator_t Begin() { return BeginIt; }
+        Iterator_t End() { return EndIt; }
+    private:
+        Iterator_t BeginIt;
+        Iterator_t EndIt;
 	};
 
-   template <typename IT> typename Poly::Range<IT>::Iterator_t begin(Poly::Range<IT>& rhs) { return rhs.Begin(); }
+    template <typename IT> typename Poly::Range<IT>::Iterator_t begin(Poly::Range<IT>& rhs) { return rhs.Begin(); }
 	template <typename IT> typename Poly::Range<IT>::Iterator_t end(Poly::Range<IT>& rhs) { return rhs.End(); }
 
-   template <typename C> auto MakeRange(C& container) { return Range<decltype( begin(std::declval<C>()) )>(container); }
-   template <typename C> auto MakeRange(const C& container) { return Range<decltype( cbegin(std::declval<C>()) )>(container); }
+    template <typename C> auto MakeRange(C& container) { return Range<decltype( begin(std::declval<C>()) )>(container); }
+    template <typename C> auto MakeRange(const C& container) { return Range<decltype( cbegin(std::declval<C>()) )>(container); }
 }
--- PolyEngine/UnitTests/Src/RangeTests.cpp
+++ PolyEngine/UnitTests/Src/RangeTests.cpp
@@ -6,185 +6,200 @@
 
 using namespace Poly;
 
-TEST_CASE("Iterator pair constructors", "Range") {
-   std::vector<int> testVec = {1, 2, 3, 4};
-   std::map<char, int> testMap = { {'a', 1}, {'b', 2}, {'c', 3} };
-   const std::vector<int>& constTestVec = testVec;
-   const std::map<char, int>& constTestMap = testMap;
+TEST_CASE("Iterator pair constructors", "Range")
+{
+    std::vector<int> testVec = {1, 2, 3, 4};
+    std::map<char, int> testMap = { {'a', 1}, {'b', 2}, {'c', 3} };
+    const std::vector<int>& constTestVec = testVec;
+    const std::map<char, int>& constTestMap = testMap;
 
-	SECTION("Mutable vector iterator pair constructor") {
-		auto range = Range(testVec.begin(), testVec.end());
+    SECTION("Mutable vector iterator pair constructor")
+    {
+	    auto range = Range(testVec.begin(), testVec.end());
       
-      int counter = 0;
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val == counter);
-      }
-      CHECK(counter == 4);
+        int counter = 0;
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val == counter);
+        }
+        CHECK(counter == 4);
 	}
 
-   SECTION("Const vector iterator pair constructor") {
-		auto range = Range(constTestVec.begin(), constTestVec.end());
+    SECTION("Const vector iterator pair constructor")
+    {
+	    auto range = Range(constTestVec.begin(), constTestVec.end());
       
-      int counter = 0;
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val == counter);
-      }
-      CHECK(counter == 4);
+        int counter = 0;
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val == counter);
+        }
+        CHECK(counter == 4);
 	}
 
-   SECTION("Mutable map iterator pair constructor") {
+    SECTION("Mutable map iterator pair constructor")
+    {
 		auto range = Range(testMap.begin(), testMap.end());
       
-      int counter = 0;
-      char keyCounter = 'a';
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val.second == counter);
-         CHECK(val.first == keyCounter);
-         ++keyCounter;
-      }
-      CHECK(counter == 3);
+        int counter = 0;
+        char keyCounter = 'a';
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val.second == counter);
+            CHECK(val.first == keyCounter);
+            ++keyCounter;
+        }
+        CHECK(counter == 3);
 	}
 
-   SECTION("Const map iterator pair constructor") {
+    SECTION("Const map iterator pair constructor")
+    {
 		auto range = Range(constTestMap.begin(), constTestMap.end());
       
-      int counter = 0;
-      char keyCounter = 'a';
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val.second == counter);
-         CHECK(val.first == keyCounter);
-         ++keyCounter;
-      }
-      CHECK(counter == 3);
+        int counter = 0;
+        char keyCounter = 'a';
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val.second == counter);
+            CHECK(val.first == keyCounter);
+            ++keyCounter;
+        }
+        CHECK(counter == 3);
 	}
 }
 
-TEST_CASE("Collection constructors", "Range") {
-   std::vector<int> testVec = {1, 2, 3, 4};
-   std::map<char, int> testMap = { {'a', 1}, {'b', 2}, {'c', 3} };
-   const std::vector<int>& constTestVec = testVec;
-   const std::map<char, int>& constTestMap = testMap;
+TEST_CASE("Collection constructors", "Range")
+{
+    std::vector<int> testVec = {1, 2, 3, 4};
+    std::map<char, int> testMap = { {'a', 1}, {'b', 2}, {'c', 3} };
+    const std::vector<int>& constTestVec = testVec;
+    const std::map<char, int>& constTestMap = testMap;
 
-	SECTION("Mutable vector collection constructor") {
+	SECTION("Mutable vector collection constructor")
+    {
 		auto range = Range<std::vector<int>::iterator>(testVec);
       
-      int counter = 0;
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val == counter);
-      }
-      CHECK(counter == 4);
+        int counter = 0;
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val == counter);
+        }
+        CHECK(counter == 4);
 	}
 
-   SECTION("Const vector collection constructor") {
+    SECTION("Const vector collection constructor")
+    {
 		auto range = Range<std::vector<int>::const_iterator>(constTestVec);
       
-      int counter = 0;
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val == counter);
-      }
-      CHECK(counter == 4);
+        int counter = 0;
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val == counter);
+        }
+        CHECK(counter == 4);
 	}
 
-   SECTION("Mutable map collection constructor") {
+    SECTION("Mutable map collection constructor")
+    {
 		auto range = Range<std::map<char, int>::iterator>(testMap);
       
-      int counter = 0;
-      char keyCounter = 'a';
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val.second == counter);
-         CHECK(val.first == keyCounter);
-         ++keyCounter;
-      }
-      CHECK(counter == 3);
+        int counter = 0;
+        char keyCounter = 'a';
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val.second == counter);
+            CHECK(val.first == keyCounter);
+            ++keyCounter;
+        }
+        CHECK(counter == 3);
 	}
 
-   SECTION("Const map collection constructor") {
+    SECTION("Const map collection constructor")
+    {
 		auto range = Range<std::map<char, int>::const_iterator>(constTestMap);
       
-      int counter = 0;
-      char keyCounter = 'a';
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val.second == counter);
-         CHECK(val.first == keyCounter);
-         ++keyCounter;
-      }
-      CHECK(counter == 3);
+        int counter = 0;
+        char keyCounter = 'a';
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val.second == counter);
+            CHECK(val.first == keyCounter);
+            ++keyCounter;
+        }
+        CHECK(counter == 3);
 	}
 }
 
-TEST_CASE("MakeRange", "Range") {
-   std::vector<int> testVec = {1, 2, 3, 4};
-   std::map<char, int> testMap = { {'a', 1}, {'b', 2}, {'c', 3} };
-   const std::vector<int>& constTestVec = testVec;
-   const std::map<char, int>& constTestMap = testMap;
+TEST_CASE("MakeRange", "Range")
+{
+    std::vector<int> testVec = {1, 2, 3, 4};
+    std::map<char, int> testMap = { {'a', 1}, {'b', 2}, {'c', 3} };
+    const std::vector<int>& constTestVec = testVec;
+    const std::map<char, int>& constTestMap = testMap;
 
-	SECTION("Mutable vector MakeRange") {
+	SECTION("Mutable vector MakeRange")
+    {
 		auto range = MakeRange(testVec);
       
-      int counter = 0;
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val == counter);
-      }
-      CHECK(counter == 4);
-	}
+        int counter = 0;
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val == counter);
+        }
+        CHECK(counter == 4);
+    }
 
-   SECTION("Const vector MakeRange") {
+    SECTION("Const vector MakeRange")
+    {
 		auto range = MakeRange(constTestVec);
       
-      int counter = 0;
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val == counter);
-      }
-      CHECK(counter == 4);
+        int counter = 0;
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val == counter);
+        }
+        CHECK(counter == 4);
 	}
 
-   SECTION("Mutable map MakeRange") {
+    SECTION("Mutable map MakeRange")
+    {
 		auto range = MakeRange(testMap);
       
-      int counter = 0;
-      char keyCounter = 'a';
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val.second == counter);
-         CHECK(val.first == keyCounter);
-         ++keyCounter;
-      }
-      CHECK(counter == 3);
+        int counter = 0;
+        char keyCounter = 'a';
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val.second == counter);
+            CHECK(val.first == keyCounter);
+            ++keyCounter;
+        }
+        CHECK(counter == 3);
 	}
 
-   SECTION("Const map MakeRange") {
+    SECTION("Const map MakeRange")
+    {
 		auto range = MakeRange(constTestMap);
       
-      int counter = 0;
-      char keyCounter = 'a';
-      for(auto val : range)
-      {
-         ++counter;
-         CHECK(val.second == counter);
-         CHECK(val.first == keyCounter);
-         ++keyCounter;
-      }
-      CHECK(counter == 3);
+        int counter = 0;
+        char keyCounter = 'a';
+        for(auto val : range)
+        {
+            ++counter;
+            CHECK(val.second == counter);
+            CHECK(val.first == keyCounter);
+            ++keyCounter;
+        }
+        CHECK(counter == 3);
 	}
 }
