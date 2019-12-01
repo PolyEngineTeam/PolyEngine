1d99d6c318a95c2d4699de10a1845d53e9aa4f60
Michał Majczak <michal.majczak92@gmail.com>
2019-12-01 13:20:39 +0100

    Engine versioning (#190)
    * Engine versioning stubs
    
    * Version tests
    
    * Test fix
    
    * Added simple version test.

--- PolyEngine/Scripts/common/project_mgr/engine_project.py
+++ PolyEngine/Scripts/common/project_mgr/engine_project.py
@@ -5,7 +5,7 @@ from .project_base import ProjectBase
 
 class EngineProject(ProjectBase):
     def __init__(self, build_postfix=None):
-        ProjectBase.__init__(self, common.SCRIPT_ENV.engine_path, 'PolyEngine', common.Version(0, 0, 1), build_postfix)
+        ProjectBase.__init__(self, common.SCRIPT_ENV.engine_path, 'PolyEngine', build_postfix)
         self._logger = common.SCRIPT_ENV.get_logger(name='EngineProject')
 
         if not os.path.isdir(self._root_path):
