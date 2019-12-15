256c93b9965de149caff7e853a3615fabf73c5ad
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2019-12-15 22:04:45 +0100

    Add test generator, replace slow functions with more effective ones

--- PolyEngine/Tests/CoreTests/Src/IterationUtilsTests.cpp
+++ PolyEngine/Tests/CoreTests/Src/IterationUtilsTests.cpp
@@ -6,36 +6,25 @@ using namespace pe::core::utils;
 
 size_t slowNextIdxCircularBuffer(size_t head, size_t tail, size_t size, size_t capacity, size_t idx)
 {
-	if(idx == capacity - 1)
+	const size_t calcIdx = (idx + 1) % capacity;
+	if (calcIdx == tail + 1) 
 	{
-		if(tail > 0)
-			return tail - 1;
-		return tail;	
+		return head;
 	}
-	else 
-	{
-		if(idx == tail - 1)	
-			return tail;
-		if(idx == tail)
-			return head;
-		return ++idx;
-	}
-    return SIZE_MAX;
+	return calcIdx;
 }
 
 size_t slowPrevIdxCircularBuffer(size_t head, size_t tail, size_t size, size_t capacity, size_t idx)
 {
-	if(idx == 0)
+	if (idx == 0)
 	{
-		return capacity - 1;
-	}
-	else
-	{
-		if(idx == head)
+		if (head > tail)
+			return capacity - 1;
+		else
 			return tail;
-		return --idx;
-	}
-    return SIZE_MAX; 
+	} else if (idx == head)
+		return tail;		
+	return idx - 1;
 }
 
 TEST_CASE("Distant head and tail, unwrapped", "[IterationUtils]")
@@ -56,6 +45,8 @@ TEST_CASE("Distant head and tail, unwrapped", "[IterationUtils]")
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 9) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 9));
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 8) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 8));
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 7) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 7));
+	CHECK(prevIdxCircularBuffer(head, size, capacity, 6) == 1);
+	CHECK(prevIdxCircularBuffer(head, size, capacity, 6) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 6));
 }
 
 TEST_CASE("Two elements of distance between head and tail, wrapped", "[IterationUtils]")
@@ -76,6 +67,8 @@ TEST_CASE("Two elements of distance between head and tail, wrapped", "[Iteration
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 5) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 5));
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 4) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 4));
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 3) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 3));
+	CHECK(prevIdxCircularBuffer(head, size, capacity, 2) == 0);
+	CHECK(prevIdxCircularBuffer(head, size, capacity, 2) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 2));
 }
 
 TEST_CASE("One element of distance between head and tail, wrapped", "[IterationUtils]")
@@ -96,6 +89,8 @@ TEST_CASE("One element of distance between head and tail, wrapped", "[IterationU
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 5) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 5));
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 4) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 4));
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 3) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 3));
+	CHECK(prevIdxCircularBuffer(head, size, capacity, 2) == 1);
+	CHECK(prevIdxCircularBuffer(head, size, capacity, 2) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 2));
 }
 
 TEST_CASE("Head at 0th index, unwrapped", "[IterationUtils]")
@@ -116,4 +111,30 @@ TEST_CASE("Head at 0th index, unwrapped", "[IterationUtils]")
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 3) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 3));
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 2) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 2));
 	CHECK(prevIdxCircularBuffer(head, size, capacity, 1) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 1));
+	CHECK(prevIdxCircularBuffer(head, size, capacity, 0) == 5);
+	CHECK(prevIdxCircularBuffer(head, size, capacity, 0) == slowPrevIdxCircularBuffer(head, tail, size, capacity, 0));
+}
+
+TEST_CASE("Iterated container of capacity 10 with growing size and moving head", "[IterationUtils]")
+{
+	// outer loop increase head position, inner loop increase size 
+	const size_t capacity = 10;
+	for(size_t i = 1; i <= capacity; ++i)
+	{
+		for(size_t j = 1; j < i; ++j)
+		{
+			const size_t head = i - 1;
+			const size_t size = j;
+			const size_t tail = (head + size) % i;	
+			size_t idx = head;
+			// iterate through whole container
+			for(size_t k = 0; k <= j; ++k)
+			{
+				INFO("Failed for values: i: " << i << " j: " << j << " k: " << k << " head: " << head << " tail: " << tail << " size: " << size << " idx: " << idx);
+				CHECK(nextIdxCircularBuffer(head, size, i, idx) == slowNextIdxCircularBuffer(head, tail, size, i, idx));
+				CHECK(prevIdxCircularBuffer(head, size, i, idx) == slowPrevIdxCircularBuffer(head, tail, size, i, idx));
+				idx = nextIdxCircularBuffer(head, size, i, idx);
+			}
+		}
+	}
 }
