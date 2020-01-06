b5f492089aff5ae5f458d624adfbcc465c87106d
Piotr <piotrek194@poczta.fm>
2020-01-07 00:34:33 +0100

    fix compile errors

--- PolyEngine/Core/Src/pe/core/storage/PriorityQueue.hpp
+++ PolyEngine/Core/Src/pe/core/storage/PriorityQueue.hpp
@@ -10,88 +10,88 @@ namespace pe::core::storage
 	public:
 		
 		PriorityQueue()
-			: priority_queue()
+			: std::priority_queue<_Ty, _Container, _Pr>()
 		{
 		}
 
 		explicit PriorityQueue(const _Pr& _Pred)
-			: priority_queue(_Pred)
+			: std::priority_queue<_Ty, _Container, _Pr>(_Pred)
 		{
 		}
 
 		PriorityQueue(const _Pr& _Pred, const _Container& _Cont)
-			: priority_queue(_Pred,_Cont)
+			: std::priority_queue<_Ty, _Container, _Pr>(_Pred,_Cont)
 		{
 		}
 
 		template<class _InIt>
 		PriorityQueue(_InIt _First, _InIt _Last)
-			: priority_queue(_First,_Last)
+			: std::priority_queue<_Ty, _Container, _Pr>(_First,_Last)
 		{	
 		}
 
 		template<class _InIt>
 		PriorityQueue(_InIt _First, _InIt _Last, const _Pr& _Pred)
-			: priority_queue(_Firest,_Last,_Pred)
+			: std::priority_queue<_Ty, _Container, _Pr>(_First,_Last,_Pred)
 		{	
 		}
 
 		template<class _InIt>
 		PriorityQueue(_InIt _First, _InIt _Last, const _Pr& _Pred, const _Container& _Cont)
-			: priority_queue(_First,_Last,_Pred,_Cont)
+			: std::priority_queue<_Ty, _Container, _Pr>(_First,_Last,_Pred,_Cont)
 		{	
 		}
 
 		template<class _Alloc,
 			class = std::enable_if_t<std::uses_allocator_v<_Container, _Alloc>>>
 			explicit PriorityQueue(const _Alloc& _Al)
-			: priority_queue(_All)
+			: std::priority_queue<_Ty, _Container, _Pr>(_All)
 		{	
 		}
 
 		template<class _Alloc,
 			class = std::enable_if_t<std::uses_allocator_v<_Container, _Alloc>>>
 			PriorityQueue(const _Pr& _Pred, const _Alloc& _Al)
-			: priority_queue(_Pred,_Al)
+			: std::priority_queue<_Ty, _Container, _Pr>(_Pred,_Al)
 		{	
 		}
 
 		template<class _Alloc,
 			class = std::enable_if_t<std::uses_allocator_v<_Container, _Alloc>>>
 			PriorityQueue(const _Pr& _Pred, const _Container& _Cont, const _Alloc& _Al)
-			: priority_queue(_Pred,_Cont,_Al)
+			: std::priority_queue<_Ty, _Container, _Pr>(_Pred,_Cont,_Al)
 		{	
 		}
 
 		template<class _Alloc,
 			class = std::enable_if_t<std::uses_allocator_v<_Container, _Alloc>>>
 			PriorityQueue(const priority_queue& _Right, const _Alloc& _Al)
-			: priority_queue(_Right,_Al)
+			: std::priority_queue<_Ty, _Container, _Pr>(_Right,_Al)
 		{	
 		}
 
 		PriorityQueue(const _Pr& _Pred, _Container&& _Cont)
-			: priority_queue(_Pred,_Cont)
+			: std::priority_queue<_Ty, _Container, _Pr>(_Pred,_Cont)
 		{
 		}
 
 		template<class _InIt>
 		PriorityQueue(_InIt _First, _InIt _Last, const _Pr& _Pred, _Container&& _Cont)
-			: priority_queue(_First,_Last,_Pred,_Cont)
+			: std::priority_queue<_Ty, _Container, _Pr>(_First,_Last,_Pred,_Cont)
 		{	
 		}
 		
 		template<class _Alloc,
 			class = std::enable_if_t<std::uses_allocator_v<_Container, _Alloc>>>
 			PriorityQueue(const _Pr& _Pred, _Container&& _Cont, const _Alloc& _Al)
-			: priority_queue(_Pred,_Cont,_Al)
+			: std::priority_queue<_Ty, _Container, _Pr>(_Pred,_Cont,_Al)
 		{	
 		}
 
 		template<class _Alloc,
 			class = std::enable_if_t<std::uses_allocator_v<_Container, _Alloc>>>
 			PriorityQueue(priority_queue&& _Right, const _Alloc& _Al)
-			: priority_queue(_Right,_Al)
+			: std::priority_queue<_Ty, _Container, _Pr>(_Right,_Al)
 		{	
 		}
 
