a24fdc9d2a633bc1d63698edf68f2d011dfbb0d7
Squares <m.piotr@moscicki.pl>
2020-01-24 18:53:56 +0100

    Add Ok and Err classes

--- PolyEngine/Core/Src/pe/core/utils/Result.hpp
+++ PolyEngine/Core/Src/pe/core/utils/Result.hpp
@@ -6,22 +6,97 @@
 namespace pe::core::utils
 {
 	//------------------------------------------------------------------------------
+	template <typename T>
+	class Ok
+	{
+	public:
+		Ok() = delete;
+		Ok(Ok&) = delete;
+		Ok(Ok&&) = default;
+		Ok& operator=(Ok&) = delete;
+		Ok& operator=(Ok&&) = delete;
+
+		explicit constexpr Ok(T val) : m_value(std::move(val)) {}
+		constexpr T&& get() && { return std::move(m_value); }
+
+	private:
+		T m_value;
+	};
+
+	//------------------------------------------------------------------------------
+	template <typename E>
+	class Err
+	{
+	public:
+		Err() = delete;
+		Err(Err&) = delete;
+		Err(Err&&) = default;
+		Err& operator=(Err&) = delete;
+		Err& operator=(Err&&) = delete;
+
+		explicit constexpr Err(E val) : m_value(std::move(val)) {}
+		constexpr E&& get() && { return std::move(m_value); }
+
+	private:
+		E m_value;
+	};
+
+	//------------------------------------------------------------------------------
+	// Result is an utility class that can be used when function can
+	// return result or error code. Its main purpose is to enable 
+	// writing exception free code. You can't call getError when Result
+	// has valid success value and you can't call getValue when Result
+	// has error value but for 
+	// @param T - type of correct value (value returned by method if 
+	//		its execution was successful
+	// @param E - type of error
 	template <typename T, typename E>
 	class Result
 	{
 	public:
+		// You can't create empty Result. Result must always store 
+		// valid value or error value 
 		Result() = delete;
+
+		// We're ok with default implementations. Some of these ctors 
+		// will be implicitly deleted if T or E aren't copy 
+		// constructible or copyable and so on.
 		Result(Result&) = default;
 		Result(Result&&) = default;
 		Result& operator=(Result&) = default;
 		Result& operator=(Result&&) = default;
-		Result(T okVal) : m_value(std::move(okVal)) {}
-		Result(E errVal) : m_value(std::move(errVal)) {}
+
+		// Ctor taking ok value. Object created with this ctor will
+		// return true from isOk and false from isErr.
+		// @param okVal - value for success result
+		constexpr Result(Ok<T> okVal) : m_value(std::move(okVal).get()) {}
+
+		// Ctor taking error value. Object created with this ctor will
+		// return false from isOk and true from isErr.
+		// @param errVal - value for error/invalid result
+		constexpr Result(Err<E> errVal) : m_value(std::move(errVal).get()) {}
 		
-		bool isOk() const { return m_value.index() == 0; }
-		bool isErr() const { return !isOk(); }
+		// @returns true if result stores valid value, false if stores 
+		//		error value
+		constexpr bool isOk() const { return m_value.index() == 0; }
+
+		// @returns false if result stores valid value, true if stores 
+		//		error value
+		constexpr bool isErr() const { return !isOk(); }
 		
-		T& getValue() { return std::get<T>(m_value); }
+		// @returns if is ok returns valid value, crashes otherwise
+		constexpr T& getValue() { return std::get<T>(m_value); }
+
+		// @returns valid value if has valid value or given alternative value if has error
+		constexpr T getValueOr(T alt) &&
+		{ 
+			if (isOk()) 
+				return std::move(getValue()); 
+			else 
+				return alt;
+		
+		}
+
 		E& getError() { return std::get<E>(m_value); }
 		const T& getValue() const { return std::get<T>(m_value); }
 		const E& getError() const { return std::get<E>(m_value); }
--- PolyEngine/Tests/CoreTests/Src/ResultTests.cpp
+++ PolyEngine/Tests/CoreTests/Src/ResultTests.cpp
@@ -21,9 +21,9 @@ namespace
 	Result<bool, eTestErrorType> func(int in)
 	{
 		if (in < 10)
-			return in > 5;
+			return Ok(in > 5);
 		else
-			return eTestErrorType::ERROR_TYPE_1;
+			return Err(eTestErrorType::ERROR_TYPE_1);
 	}
 
 	int func5(int a, float b, Result<bool, eTestErrorType> c, float d, std::string e)
@@ -34,10 +34,10 @@ namespace
 
 TEST_CASE("Result.isOk", "[Result]") 
 {
-	Result<int, eTestErrorType> result1 = 43;
+	Result<int, eTestErrorType> result1 = Ok(43);
 	REQUIRE(result1.isOk() == true);
 
-	Result<int, eTestErrorType> result2 = eTestErrorType::ERROR_TYPE_1;
+	Result<int, eTestErrorType> result2 = Err(eTestErrorType::ERROR_TYPE_1);
 	REQUIRE(result2.isOk() == false);
 
 	Result<void, eTestErrorType> result3;
@@ -49,10 +49,10 @@ TEST_CASE("Result.isOk", "[Result]")
 
 TEST_CASE("Result.isErr", "[Result]")
 {
-	Result<int, eTestErrorType> result1 = 43;
+	Result<int, eTestErrorType> result1 = Ok(43);
 	REQUIRE(result1.isErr() == false);
 
-	Result<int, eTestErrorType> result2 = eTestErrorType::ERROR_TYPE_1;
+	Result<int, eTestErrorType> result2 = Err(eTestErrorType::ERROR_TYPE_1);
 	REQUIRE(result2.isErr() == true);
 
 	Result<void, eTestErrorType> result3;
@@ -64,152 +64,152 @@ TEST_CASE("Result.isErr", "[Result]")
 
 TEST_CASE("Result.getValue", "[Result]")
 {
-	Result<int, eTestErrorType> result1 = 43;
+	Result<int, eTestErrorType> result1 = Ok(43);
 	REQUIRE(result1.getValue() == 43);
 }
 
 TEST_CASE("Result.getError", "[Result]")
 {
-	Result<int, eTestErrorType> result2 = eTestErrorType::ERROR_TYPE_1;
+	Result<int, eTestErrorType> result2 = Err(eTestErrorType::ERROR_TYPE_1);
 	REQUIRE(result2.getError() == eTestErrorType::ERROR_TYPE_1);
 
 	Result<void, eTestErrorType> result4 = eTestErrorType::ERROR_TYPE_1;
 	REQUIRE(result4.getError() == eTestErrorType::ERROR_TYPE_1);
 }
-
-TEST_CASE("Result.join", "[Result]")
-{
-	Result<Result<int, eTestErrorType>, eTestErrorType> result1 = 43;
-	REQUIRE(result::join(result1).getValue() == 43);
-
-	Result<Result<int, eTestErrorType>, eTestErrorType> result2 = Result<int, eTestErrorType>(eTestErrorType::ERROR_TYPE_1);
-	REQUIRE(result::join(result2).getError() == eTestErrorType::ERROR_TYPE_1);
-	
-	Result<Result<void, eTestErrorType>, eTestErrorType> result3 = Result<void, eTestErrorType>();
-	REQUIRE(result::join(result3).isOk() == true);
-	
-	Result<Result<void, eTestErrorType>, eTestErrorType> result4 = eTestErrorType::ERROR_TYPE_1;
-	REQUIRE(result::join(result4).isOk() == false);
-
-	Result<Result<void, eTestErrorType>, eTestErrorType> result5 = eTestErrorType::ERROR_TYPE_1;
-	REQUIRE(result::join(result4).getError() == eTestErrorType::ERROR_TYPE_1);
-}
-
-TEST_CASE("Result.bind", "[Result]")
-{
-	Result<int, eTestErrorType> result1 = 2;
-	REQUIRE(result::bind(result1, func).getValue() == false);
-
-	Result<int, eTestErrorType> result2 = 7;
-	REQUIRE(result::bind(result2, func).getValue() == true);
-
-	Result<int, eTestErrorType> result3 = 11;
-	REQUIRE(result::bind(result3, func).getError() == eTestErrorType::ERROR_TYPE_1);
-
-	Result<int, eTestErrorType> result4 = eTestErrorType::ERROR_TYPE_2;
-	REQUIRE(result::bind(result4, func).getError() == eTestErrorType::ERROR_TYPE_2);
-}
-
-TEST_CASE("Result.first", "[Result]")
-{
-	{
-		Result<int, eTestErrorType> result1 = 2;
-		Result<bool, eTestErrorType> result2 = true;
-		REQUIRE(result1.first(result2).getValue() == true);
-
-		Result<int, eTestErrorType> result3 = eTestErrorType::ERROR_TYPE_1;
-		Result<bool, eTestErrorType> result4 = true;
-		REQUIRE(result3.first(result4).getError() == eTestErrorType::ERROR_TYPE_1);
-
-		Result<bool, eTestErrorType> result5 = true;
-		Result<int, eTestErrorType> result6 = eTestErrorType::ERROR_TYPE_2;
-		REQUIRE(result5.first(result6).getError() == eTestErrorType::ERROR_TYPE_2);
-
-		Result<bool, eTestErrorType> result7 = eTestErrorType::ERROR_TYPE_1;
-		Result<int, eTestErrorType> result8 = eTestErrorType::ERROR_TYPE_2;
-		REQUIRE(result7.first(result8).getError() == eTestErrorType::ERROR_TYPE_1);
-	}
-	{
-		Result<void, eTestErrorType> result1;
-		Result<bool, eTestErrorType> result2 = true;
-		REQUIRE(result1.first(result2).getValue() == true);
-
-		Result<void, eTestErrorType> result3 = eTestErrorType::ERROR_TYPE_1;
-		Result<bool, eTestErrorType> result4 = true;
-		REQUIRE(result3.first(result4).getError() == eTestErrorType::ERROR_TYPE_1);
-
-		Result<void, eTestErrorType> result5;
-		Result<int, eTestErrorType> result6 = eTestErrorType::ERROR_TYPE_2;
-		REQUIRE(result5.first(result6).getError() == eTestErrorType::ERROR_TYPE_2);
-
-		Result<void, eTestErrorType> result7 = eTestErrorType::ERROR_TYPE_1;
-		Result<int, eTestErrorType> result8 = eTestErrorType::ERROR_TYPE_2;
-		REQUIRE(result7.first(result8).getError() == eTestErrorType::ERROR_TYPE_1);
-	}
-	{
-		Result<int, eTestErrorType> result1 = 2;
-		Result<void, eTestErrorType> result2;
-		REQUIRE(result1.first(result2).isOk() == true);
-
-		Result<int, eTestErrorType> result3 = eTestErrorType::ERROR_TYPE_1;
-		Result<void, eTestErrorType> result4;
-		REQUIRE(result3.first(result4).getError() == eTestErrorType::ERROR_TYPE_1);
-
-		Result<bool, eTestErrorType> result5 = true;
-		Result<void, eTestErrorType> result6 = eTestErrorType::ERROR_TYPE_2;
-		REQUIRE(result5.first(result6).getError() == eTestErrorType::ERROR_TYPE_2);
-
-		Result<bool, eTestErrorType> result7 = eTestErrorType::ERROR_TYPE_1;
-		Result<void, eTestErrorType> result8 = eTestErrorType::ERROR_TYPE_2;
-		REQUIRE(result7.first(result8).getError() == eTestErrorType::ERROR_TYPE_1);
-	}
-}
-
-TEST_CASE("Result.last", "[Result]")
-{
-	{
-		Result<int, eTestErrorType> result1 = 2;
-		Result<int, eTestErrorType> result2 = true;
-		REQUIRE(result1.last(result2).getValue() == 2);
-
-		Result<int, eTestErrorType> result3 = eTestErrorType::ERROR_TYPE_1;
-		Result<int, eTestErrorType> result4 = 3;
-		REQUIRE(result3.last(result4).getValue() == 3);
-	
-		Result<int, eTestErrorType> result5 = 4;
-		Result<int, eTestErrorType> result6 = eTestErrorType::ERROR_TYPE_2;
-		REQUIRE(result5.last(result6).getValue() == 4);
-	
-		Result<int, eTestErrorType> result7 = eTestErrorType::ERROR_TYPE_1;
-		Result<int, eTestErrorType> result8 = eTestErrorType::ERROR_TYPE_2;
-		REQUIRE(result7.last(result8).getError() == eTestErrorType::ERROR_TYPE_2);
-	}
-	{
-		Result<void, eTestErrorType> result1;
-		Result<void, eTestErrorType> result2;
-		REQUIRE(result1.last(result2).isOk() == true);
-	
-		Result<void, eTestErrorType> result3 = eTestErrorType::ERROR_TYPE_1;
-		Result<void, eTestErrorType> result4;
-		REQUIRE(result3.last(result4).isOk() == true);
-	
-		Result<void, eTestErrorType> result5;
-		Result<void, eTestErrorType> result6 = eTestErrorType::ERROR_TYPE_2;
-		REQUIRE(result5.last(result6).isOk() == true);
-		
-		Result<void, eTestErrorType> result7 = eTestErrorType::ERROR_TYPE_1;
-		Result<void, eTestErrorType> result8 = eTestErrorType::ERROR_TYPE_2;
-		REQUIRE(result7.last(result8).getError() == eTestErrorType::ERROR_TYPE_2);
-	}
-}
-
-TEST_CASE("Result.lift", "[Result]")
-{
-	//LiftedFuncFactory::
-	auto method = result::lift<eTestErrorType>(&simpleFunc);
-	auto method2 = result::lift<eTestErrorType>(&func);
-	auto method3 = result::lift<eTestErrorType>(&func5);
-	method();
-	method2(3);
-	method3(1, 2.f, Result<bool, eTestErrorType>(false), 4.f, std::string("a"));
-}
+//
+//TEST_CASE("Result.join", "[Result]")
+//{
+//	Result<Result<int, eTestErrorType>, eTestErrorType> result1 = Result<int, eTestErrorType>(43);
+//	REQUIRE(result::join(result1).getValue() == 43);
+//
+//	Result<Result<int, eTestErrorType>, eTestErrorType> result2 = Result<int, eTestErrorType>(eTestErrorType::ERROR_TYPE_1);
+//	REQUIRE(result::join(result2).getError() == eTestErrorType::ERROR_TYPE_1);
+//	
+//	Result<Result<void, eTestErrorType>, eTestErrorType> result3 = Result<void, eTestErrorType>();
+//	REQUIRE(result::join(result3).isOk() == true);
+//	
+//	Result<Result<void, eTestErrorType>, eTestErrorType> result4 = eTestErrorType::ERROR_TYPE_1;
+//	REQUIRE(result::join(result4).isOk() == false);
+//
+//	Result<Result<void, eTestErrorType>, eTestErrorType> result5 = eTestErrorType::ERROR_TYPE_1;
+//	REQUIRE(result::join(result4).getError() == eTestErrorType::ERROR_TYPE_1);
+//}
+//
+//TEST_CASE("Result.bind", "[Result]")
+//{
+//	Result<int, eTestErrorType> result1 = 2;
+//	REQUIRE(result::bind(result1, func).getValue() == false);
+//
+//	Result<int, eTestErrorType> result2 = 7;
+//	REQUIRE(result::bind(result2, func).getValue() == true);
+//
+//	Result<int, eTestErrorType> result3 = 11;
+//	REQUIRE(result::bind(result3, func).getError() == eTestErrorType::ERROR_TYPE_1);
+//
+//	Result<int, eTestErrorType> result4 = eTestErrorType::ERROR_TYPE_2;
+//	REQUIRE(result::bind(result4, func).getError() == eTestErrorType::ERROR_TYPE_2);
+//}
+//
+//TEST_CASE("Result.first", "[Result]")
+//{
+//	{
+//		Result<int, eTestErrorType> result1 = 2;
+//		Result<bool, eTestErrorType> result2 = true;
+//		REQUIRE(result1.first(result2).getValue() == true);
+//
+//		Result<int, eTestErrorType> result3 = eTestErrorType::ERROR_TYPE_1;
+//		Result<bool, eTestErrorType> result4 = true;
+//		REQUIRE(result3.first(result4).getError() == eTestErrorType::ERROR_TYPE_1);
+//
+//		Result<bool, eTestErrorType> result5 = true;
+//		Result<int, eTestErrorType> result6 = eTestErrorType::ERROR_TYPE_2;
+//		REQUIRE(result5.first(result6).getError() == eTestErrorType::ERROR_TYPE_2);
+//
+//		Result<bool, eTestErrorType> result7 = eTestErrorType::ERROR_TYPE_1;
+//		Result<int, eTestErrorType> result8 = eTestErrorType::ERROR_TYPE_2;
+//		REQUIRE(result7.first(result8).getError() == eTestErrorType::ERROR_TYPE_1);
+//	}
+//	{
+//		Result<void, eTestErrorType> result1;
+//		Result<bool, eTestErrorType> result2 = true;
+//		REQUIRE(result1.first(result2).getValue() == true);
+//
+//		Result<void, eTestErrorType> result3 = eTestErrorType::ERROR_TYPE_1;
+//		Result<bool, eTestErrorType> result4 = true;
+//		REQUIRE(result3.first(result4).getError() == eTestErrorType::ERROR_TYPE_1);
+//
+//		Result<void, eTestErrorType> result5;
+//		Result<int, eTestErrorType> result6 = eTestErrorType::ERROR_TYPE_2;
+//		REQUIRE(result5.first(result6).getError() == eTestErrorType::ERROR_TYPE_2);
+//
+//		Result<void, eTestErrorType> result7 = eTestErrorType::ERROR_TYPE_1;
+//		Result<int, eTestErrorType> result8 = eTestErrorType::ERROR_TYPE_2;
+//		REQUIRE(result7.first(result8).getError() == eTestErrorType::ERROR_TYPE_1);
+//	}
+//	{
+//		Result<int, eTestErrorType> result1 = 2;
+//		Result<void, eTestErrorType> result2;
+//		REQUIRE(result1.first(result2).isOk() == true);
+//
+//		Result<int, eTestErrorType> result3 = eTestErrorType::ERROR_TYPE_1;
+//		Result<void, eTestErrorType> result4;
+//		REQUIRE(result3.first(result4).getError() == eTestErrorType::ERROR_TYPE_1);
+//
+//		Result<bool, eTestErrorType> result5 = true;
+//		Result<void, eTestErrorType> result6 = eTestErrorType::ERROR_TYPE_2;
+//		REQUIRE(result5.first(result6).getError() == eTestErrorType::ERROR_TYPE_2);
+//
+//		Result<bool, eTestErrorType> result7 = eTestErrorType::ERROR_TYPE_1;
+//		Result<void, eTestErrorType> result8 = eTestErrorType::ERROR_TYPE_2;
+//		REQUIRE(result7.first(result8).getError() == eTestErrorType::ERROR_TYPE_1);
+//	}
+//}
+//
+//TEST_CASE("Result.last", "[Result]")
+//{
+//	{
+//		Result<int, eTestErrorType> result1 = 2;
+//		Result<int, eTestErrorType> result2 = true;
+//		REQUIRE(result1.last(result2).getValue() == 2);
+//
+//		Result<int, eTestErrorType> result3 = eTestErrorType::ERROR_TYPE_1;
+//		Result<int, eTestErrorType> result4 = 3;
+//		REQUIRE(result3.last(result4).getValue() == 3);
+//	
+//		Result<int, eTestErrorType> result5 = 4;
+//		Result<int, eTestErrorType> result6 = eTestErrorType::ERROR_TYPE_2;
+//		REQUIRE(result5.last(result6).getValue() == 4);
+//	
+//		Result<int, eTestErrorType> result7 = eTestErrorType::ERROR_TYPE_1;
+//		Result<int, eTestErrorType> result8 = eTestErrorType::ERROR_TYPE_2;
+//		REQUIRE(result7.last(result8).getError() == eTestErrorType::ERROR_TYPE_2);
+//	}
+//	{
+//		Result<void, eTestErrorType> result1;
+//		Result<void, eTestErrorType> result2;
+//		REQUIRE(result1.last(result2).isOk() == true);
+//	
+//		Result<void, eTestErrorType> result3 = eTestErrorType::ERROR_TYPE_1;
+//		Result<void, eTestErrorType> result4;
+//		REQUIRE(result3.last(result4).isOk() == true);
+//	
+//		Result<void, eTestErrorType> result5;
+//		Result<void, eTestErrorType> result6 = eTestErrorType::ERROR_TYPE_2;
+//		REQUIRE(result5.last(result6).isOk() == true);
+//		
+//		Result<void, eTestErrorType> result7 = eTestErrorType::ERROR_TYPE_1;
+//		Result<void, eTestErrorType> result8 = eTestErrorType::ERROR_TYPE_2;
+//		REQUIRE(result7.last(result8).getError() == eTestErrorType::ERROR_TYPE_2);
+//	}
+//}
+//
+//TEST_CASE("Result.lift", "[Result]")
+//{
+//	//LiftedFuncFactory::
+//	auto method = result::lift<eTestErrorType>(&simpleFunc);
+//	auto method2 = result::lift<eTestErrorType>(&func);
+//	auto method3 = result::lift<eTestErrorType>(&func5);
+//	method();
+//	method2(3);
+//	method3(1, 2.f, Result<bool, eTestErrorType>(false), 4.f, std::string("a"));
+//}
